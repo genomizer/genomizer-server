b4e7ef99d0a686042d045b4b8acda244b6944801
skraj <sjogreneric@live.se>
2014-04-25 13:11:27 +0200

    filethread

---
+++ src/transfer/Command_Object.java
@@ -0,0 +1,14 @@
+package transfer;
+
+/**
+ * Project: genomizer-Server
+ * Package: transfer
+ * User: c08esn
+ * Date: 4/25/14
+ * Time: 12:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Command_Object {
+
+    String id= "123";
+}
