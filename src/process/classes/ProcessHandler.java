b1cc6df7987daf6d0bf22b5bd74b2f78786e3808
Oskar Abrahamsson <c11oan@cs.umu.se>
2014-04-25 13:46:04 +0200

    adding methods to processHandler

--- src/process/classes/ProcessHandler.java
+++ src/process/classes/ProcessHandler.java
@@ -1,11 +1,22 @@
 package process.classes;
 
 public class ProcessHandler {
+
 	private ProcessHandler() {
-		
+
 	}
 	public static ProcessHandler createProcessHandler(){
 		return new ProcessHandler();
 	}
-	
+
+	public void runExecutable(String process, String[] param, String inFilePath, String outFilePath) throws ProcessException {
+
+	}
+
+	public void runConversion(String conversion, String inFilePath, String outFilePath){
+
+	}
+
+
+
 }
