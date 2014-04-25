9d123bc31ec06dd9adddee128dbc93b8a1ff12c7
skraj <sjogreneric@live.se>
2014-04-25 13:49:20 +0200

    Added UploadCommand class and test

---
+++ src/transfer/Test/UploadCommandTest.java
@@ -0,0 +1,22 @@
+package transfer.Test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Project: genomizer-Server
+ * Package: transfer.Test
+ * User: c08esn
+ * Date: 4/25/14
+ * Time: 1:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UploadCommandTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+
+}
