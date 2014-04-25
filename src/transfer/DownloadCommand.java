0ae18be3b7c7120ad93208eecd03f5f217296a32
oi11msd <oi11msd@cs.umu.se>
2014-04-25 14:01:11 +0200

    Added methods to DownloadCommand

--- src/transfer/DownloadCommand.java
+++ src/transfer/DownloadCommand.java
@@ -1,18 +1,39 @@
 package transfer;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class DownloadCommand extends Command {
 
+	private String filePath;
 
+	public DownloadCommand(String filePath) {
+		this.filePath = filePath;
+
+	}
 
 
 	public void Execute() {
-		
-		
-		
-		
+
+
+
+
 	}
 
-	
-	
-	
+	private String urlFactory() {
+		String url;
+
+		String host = null;
+		try {
+			host = InetAddress.getLocalHost().getCanonicalHostName();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+
+		url = "file://" + host + "/" + filePath;
+
+		return url;
+	}
+
+
 }
