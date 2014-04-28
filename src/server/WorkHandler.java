a5918e85682192bb097215647a8c026121d867f2
splutt <c11rog@cs.umu.se>
2014-04-28 16:44:21 +0200

    Added WorkHandler class and two test files

---
+++ src/server/WorkHandler.java
@@ -0,0 +1,57 @@
+package server;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import server.test.CommandMock;
+
+
+
+
+
+
+public class WorkHandler extends Thread{
+
+	private Queue<CommandMock> workQueue;
+
+	public WorkHandler(){
+		workQueue = new LinkedList<CommandMock>();
+	}
+
+	public void addWork(CommandMock commandMock) {
+		workQueue.add(commandMock);
+
+	}
+
+	@Override
+	public void run(){
+		System.out.println(Thread.currentThread().getName());
+
+		while(true){
+			if(!workQueue.isEmpty()){
+
+				CommandMock work = workQueue.poll();
+				work.execute();
+			}
+		}
+
+	}
+
+	public static void main(String [] args){
+		WorkHandler workHandler = new WorkHandler();
+		System.out.println(Thread.currentThread().getName());
+		workHandler.start();
+
+
+			workHandler.addWork(new CommandMock("lol"));
+			workHandler.addWork(new CommandMock("lol"));
+			workHandler.addWork(new CommandMock("lol"));
+			workHandler.addWork(new CommandMock("lol"));
+
+
+	}
+
+
+}
+
