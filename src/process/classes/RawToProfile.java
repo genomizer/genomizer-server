a53ac8b98f19574646ff95e93b092d9ab5997796
Christer Jakobsson <dv12cjn@cs.umu.se>
2014-04-28 15:50:58 +0200

    Created new files

---
+++ src/process/classes/RawToProfile.java
@@ -0,0 +1,18 @@
+package process.classes;
+
+import java.io.IOException;
+
+public class RawToProfile extends Executor {
+
+		//Testar bowtie. Sparar ner resultaten i test.map.
+//		String[] com = new String[]{"bowtie","-a","-m","1","--best","-p","10","-v","2","d_melanogaster_fb5_22","-q","reads/MOF_male_wt_reads_sample.fastq","-S","test/male.sam"};
+//		String[] com = new String[]{"bowtie","e_coli", "reads/e_coli_1000.fq"};
+
+		public void standardParamProcedure(String[] strings) {
+		}
+
+		public void specificParamProcedure(String[] strings) {
+			// TODO Auto-generated method stub
+
+		}
+}
---
+++ src/process/tests/RawToProfileTest.java
@@ -0,0 +1,45 @@
+package process.tests;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+
+import junit.framework.Assert;
+import junit.framework.AssertionFailedError;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import process.classes.Executor;
+import process.classes.RawToProfile;
+
+public class RawToProfileTest {
+	RawToProfile rtp = null;
+
+	@Before
+	public void setup() {
+		rtp = new RawToProfile();
+	}
+
+	@After
+	public void tearDown() {
+		rtp = null;
+	}
+
+//	@Test(expected = IOException.class)
+//	public void ExceptedIOException() {
+//		rtp.execute(new String[]{"HEJ"});
+//	}
+
+	@Test
+	public void StandardShouldGetString() {
+		rtp.standardParamProcedure(new String[]{""});
+	}
+
+	@Test
+	public void SpecificShouldGetString() {
+		rtp.specificParamProcedure(new String[]{""});
+	}
+}
