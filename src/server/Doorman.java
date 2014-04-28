9007bd4060eefab1c1a015c60d1b082d1fbf7cd6
boffin <olov.langstrom@gmail.com>
2014-04-28 15:19:59 +0200

    doorman

--- src/server/Doorman.java
+++ src/server/Doorman.java
@@ -1,12 +1,61 @@
 package server;
 
-import server.test.CommandHandlerDummy;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executor;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import server.test.CommandHandler;
+
+
 
 public class Doorman {
 
-	public Doorman(CommandHandlerDummy commandHandlerDummy, int port) {
-		// TODO Auto-generated constructor stub
-		
+	private HttpServer httpServer;
+	private CommandHandler commandHandler;
+
+	public static final int LOGIN_COMMAND = 1;
+	public static final int EXPERIMENT_COMMAND = 2;
+	public static final int FILE_COMMAND = 3;
+	public static final int SEARCH_COMMAND = 4;
+	public static final int USER_COMMAND = 5;
+	public static final int PROCESS_COMMAND = 6;
+	public static final int SYSADM_COMMAND = 7;
+
+	public Doorman(CommandHandler commandHandler, int port) throws IOException {
+
+		this.commandHandler = commandHandler;
+
+		httpServer = HttpServer.create(new InetSocketAddress(port),0);
+		//httpServer.createContext("/", rootHandler());
+		httpServer.createContext("/login", rootHandler(LOGIN_COMMAND));
+		httpServer.createContext("/experiment", rootHandler(EXPERIMENT_COMMAND));
+		httpServer.createContext("/file", rootHandler(FILE_COMMAND));
+		httpServer.createContext("/search", rootHandler(SEARCH_COMMAND));
+		httpServer.createContext("/user", rootHandler(USER_COMMAND));
+		httpServer.createContext("/process", rootHandler(PROCESS_COMMAND));
+		httpServer.createContext("/sysadm", rootHandler(SYSADM_COMMAND));
+
+		httpServer.setExecutor(new Executor() {
+			@Override
+			public void execute(Runnable command) {
+				new Thread(command).start();
+			}
+		});
 	}
 
+	public void start() {
+		httpServer.start();
+	}
+
+	HttpHandler rootHandler(int code) {
+		return new HttpHandler() {
+			@Override
+			public void handle(HttpExchange arg0) throws IOException {
+				commandHandler.doStuff(code);
+			}
+		};
+	}
 }
--- src/server/test/DoormanTest.java
+++ src/server/test/DoormanTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 
 import org.junit.Before;
@@ -15,30 +16,22 @@ import server.Doorman;
 public class DoormanTest {
 
 	private Doorman doorman;
-	
+
 	@Before
 	public void setUp() throws Exception {
 		doorman = new Doorman(new CommandHandlerDummy(), 8080);
+		doorman.start();
 	}
-	
-	
+
 
 	@Test
-	public void shouldConnectOn8080() {
-		URL url;
-		HttpURLConnection httpcon = null;
-		try {
-			url = new URL("http://localhost:8000/test");
-			httpcon = (HttpURLConnection) url.openConnection();
-		} catch (IOException e) {
-			fail();
-		}
-		try {
-			httpcon.connect();
-		} catch (IOException e) {
-			fail();
-		}
-		assertNotNull(httpcon);
+	public void shouldAcceptRequests() throws Exception {
+		HttpURLConnection httpcon = request("");
+		httpcon.getResponseCode();
 	}
 
+	private HttpURLConnection request(String path) throws Exception {
+		URL url = new URL("http://localhost:8080/" + path);
+		return (HttpURLConnection) url.openConnection();
+	}
 }
