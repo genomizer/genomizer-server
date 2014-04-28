1bf1a7ce14b705b7e7208750d6d556f30593f3dc
tfy09jnn <tfy09jnn@outlook.com>
2014-04-28 14:43:22 +0200

    Temporary placeholders.... download gson to compile...... https://code.google.com/p/google-gson/downloads/detail?name=google-gson-2.2.4-release.zip

---
+++ src/command/Command.java
@@ -0,0 +1,34 @@
+package command;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+/**
+ * Class used to represent a command in the software.
+ *
+ * @author Jonas Nilsson
+ * @version 1.0
+ */
+public abstract class Command {
+
+	//Used to validate the class object.
+	public abstract boolean validate();
+
+	//Initiate JSON. (Remove later)
+	public abstract void initiateJson(String[] values);
+
+	//Method used to run command.
+	public abstract void execute();
+
+	//Creates a JSON String.
+	public String createJson() {
+
+		//Create the builder.
+	    final GsonBuilder builder = new GsonBuilder();
+	    builder.excludeFieldsWithoutExposeAnnotation();
+	    final Gson gson = builder.create();
+
+		return gson.toJson(this);
+	}
+
+}
