33af1dd53b7786dfe34232c770260659d17264e5
Jonas M <Jonas M@130.239.225.238>
2014-04-28 14:36:07 +0200

    tests

---
+++ src/server/Doorman.java
@@ -0,0 +1,12 @@
+package server;
+
+import server.test.CommandHandlerDummy;
+
+public class Doorman {
+
+	public Doorman(CommandHandlerDummy commandHandlerDummy, int port) {
+		// TODO Auto-generated constructor stub
+		
+	}
+
+}
