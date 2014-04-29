e1f0511409b2d82e14ccbfa5c8ca93299223dcee
c11jmm <c11jmm@cyberman.cs.umu.se>
2014-04-29 15:55:49 +0200

    Doorman ClientDummy

--- src/server/test/ClientDummy.java
+++ src/server/test/ClientDummy.java
@@ -12,27 +12,20 @@ import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.UUID;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import com.google.gson.*;
 
 public class ClientDummy {
 
-
-	private final String USER_AGENT = "Mozilla/5.0";
-
-	public static void main(String[] args) throws Exception {
-
-		ClientDummy http = new ClientDummy();
-
-		System.out.println("Testing 1 - Send Http GET request");
-		http.sendLogin();
-
-		//System.out.println("\nTesting 2 - Send Http POST request");
-		//http.sendPost();
-
+	public static void main(String args[]) throws Exception {
+		sendLogin();
 	}
 
-	// HTTP GET request
-	private void sendLogin() throws Exception {
+	private static void sendLogin() throws Exception {
 
 		String url = "http://localhost:8080/login";
 
@@ -44,62 +37,28 @@ public class ClientDummy {
 
 		//add request header
 		con.setRequestProperty("Content-Type", "application/json");
+		con.setRequestProperty("Authorization", UUID.randomUUID().toString());
 
 		JsonObject jj=new JsonObject();
 		jj.addProperty("username", "jonas");
 		jj.addProperty("password", "losenord");
 
+		System.out.println(String.valueOf(jj.toString().getBytes().length));
+//		con.setRequestProperty("Content-Length", String.valueOf(jj.toString().getBytes().length));
+
 		System.out.println(jj.toString());
+
+		String json_output = jj.toString();
 
 		con.setDoOutput(true);
 		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-		wr.write(jj.toString().getBytes());
+		wr.write(json_output.getBytes());
 		wr.flush();
 		wr.close();
+
 
 		int responseCode = con.getResponseCode();
 		System.out.println("\nSending 'GET' request to URL : " + url);
-		System.out.println("Response Code : " + responseCode);
-
-		BufferedReader in = new BufferedReader(
-		        new InputStreamReader(con.getInputStream()));
-		String inputLine;
-		StringBuffer response = new StringBuffer();
-
-		while ((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
-		}
-		in.close();
-
-		//print result
-		System.out.println(response.toString());
-
-	}
-
-	// HTTP POST request
-	private void sendPost() throws Exception {
-
-		String url = "http://localhost:8080/login";
-		URL obj = new URL(url);
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-
-		//add reuqest header
-		con.setRequestMethod("POST");
-		con.setRequestProperty("User-Agent", USER_AGENT);
-		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
-
-		String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
-
-		// Send post request
-		con.setDoOutput(true);
-		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-		wr.writeBytes(urlParameters);
-		wr.flush();
-		wr.close();
-
-		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'POST' request to URL : " + url);
-		System.out.println("Post parameters : " + urlParameters);
 		System.out.println("Response Code : " + responseCode);
 
 		BufferedReader in = new BufferedReader(
