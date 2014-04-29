e6ad67e5666daba2c642532871627d9df1989f36
c11vlg <walkma@walkma-linux>
2014-04-29 16:21:36 +0200

    added auth to php upload

--- php-scripts/upload.php
+++ php-scripts/upload.php
@@ -2,6 +2,15 @@
 
 #$destination_path = "/var/www/walkma.no-ip.info/results/";
 
+if (!isset($_SERVER['PHP_AUTH_USER'])) {
+    header('WWW-Authenticate: Basic realm="My Realm"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo 'Text to send if user hits Cancel button';
+    exit;
+} else {
+    echo "<p>Hello {$_SERVER['PHP_AUTH_USER']}.</p>";
+    echo "<p>You entered {$_SERVER['PHP_AUTH_PW']} as your password.</p>";
+}
 
 $file_name = basename($_POST['data']);
 $destination_path = dirname($_POST['data']) . "/";
