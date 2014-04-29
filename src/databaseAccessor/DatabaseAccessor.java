85bc6f3338a3d60af25b53da25f6ba0e3837f380
Ruaridh Watt <ruaridh.watt@gmail.com>
2014-04-29 14:33:00 +0200

    Adding drop down annotations now possible

--- src/databaseAccessor/DatabaseAccessor.java
+++ src/databaseAccessor/DatabaseAccessor.java
@@ -1,4 +1,5 @@
 package databaseAccessor;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -10,18 +11,17 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
-
 public class DatabaseAccessor {
 
     Connection conn;
 
-    public DatabaseAccessor(String username, String password, String host, String database) throws SQLException {
+    public DatabaseAccessor(String username, String password, String host,
+            String database) throws SQLException {
 
         String url = "jdbc:postgresql://" + host + "/" + database;
         Properties props = new Properties();
         props.setProperty("user", username);
         props.setProperty("password", password);
-
 
         conn = DriverManager.getConnection(url, props);
 
@@ -31,10 +31,10 @@ public class DatabaseAccessor {
         return conn != null;
     }
 
-    public void addUser(String username, String password, String role) throws SQLException {
-        String userString = "INSERT INTO User_Info " +
-                "(Username, Password, Role) VALUES " +
-                "(?, ?, ?)";
+    public void addUser(String username, String password, String role)
+            throws SQLException {
+        String userString = "INSERT INTO User_Info "
+                + "(Username, Password, Role) VALUES " + "(?, ?, ?)";
         PreparedStatement addUser = conn.prepareStatement(userString);
         addUser.setString(1, username);
         addUser.setString(2, password);
@@ -57,8 +57,7 @@ public class DatabaseAccessor {
 
     public void deleteUser(String username) throws SQLException {
 
-        String statementStr = "DELETE FROM User_Info " +
-                "WHERE (Username = ?)";
+        String statementStr = "DELETE FROM User_Info " + "WHERE (Username = ?)";
         PreparedStatement deleteUser = conn.prepareStatement(statementStr);
         deleteUser.setString(1, username);
         deleteUser.executeUpdate();
@@ -69,20 +68,21 @@ public class DatabaseAccessor {
     }
 
     public String getPassword(String testUser) throws SQLException {
-        String query = "SELECT Password FROM User_Info " +
-        		"WHERE (Username = ?)";
+        String query = "SELECT Password FROM User_Info "
+                + "WHERE (Username = ?)";
         PreparedStatement getPassword = conn.prepareStatement(query);
         getPassword.setString(1, testUser);
         ResultSet rs = getPassword.executeQuery();
-        if(rs.next()) {// TODO Auto-generated method stub
+        if (rs.next()) {// TODO Auto-generated method stub
             return rs.getString("password");
         }
         return null;
     }
 
-    public int resetPassword(String username, String newPassword) throws SQLException {
-        String query = "UPDATE User_Info SET Password = ? " +
-        		"WHERE (Username = ?)";
+    public int resetPassword(String username, String newPassword)
+            throws SQLException {
+        String query = "UPDATE User_Info SET Password = ? "
+                + "WHERE (Username = ?)";
         PreparedStatement resetPassword = conn.prepareStatement(query);
         resetPassword.setString(1, newPassword);
         resetPassword.setString(2, username);
@@ -91,8 +91,8 @@ public class DatabaseAccessor {
     }
 
     public int setRole(String username, String role) throws SQLException {
-        String query = "UPDATE User_Info SET Role = ? " +
-                "WHERE (Username = ?)";
+        String query = "UPDATE User_Info SET Role = ? "
+                + "WHERE (Username = ?)";
         PreparedStatement setRole = conn.prepareStatement(query);
         setRole.setString(1, role);
         setRole.setString(2, username);
@@ -100,20 +100,19 @@ public class DatabaseAccessor {
     }
 
     public String getRole(String username) throws SQLException {
-        String query = "SELECT Role FROM User_Info " +
-                "WHERE (Username = ?)";
+        String query = "SELECT Role FROM User_Info " + "WHERE (Username = ?)";
         PreparedStatement getRole = conn.prepareStatement(query);
         getRole.setString(1, username);
         ResultSet rs = getRole.executeQuery();
-        if(rs.next()) {
+        if (rs.next()) {
             return rs.getString("Role");
         }
         return null;
     }
 
     public int addFreeTextAnnotation(String label) throws SQLException {
-        String query = "INSERT INTO Annotation " +
-        		"(Label, DataType) VALUES (?, 'FreeText')";
+        String query = "INSERT INTO Annotation "
+                + "(Label, DataType) VALUES (?, 'FreeText')";
         PreparedStatement addAnnotation = conn.prepareStatement(query);
         addAnnotation.setString(1, label);
         return addAnnotation.executeUpdate();
@@ -133,11 +132,67 @@ public class DatabaseAccessor {
     }
 
     public int deleteAnnotation(String label) throws SQLException {
-        String statementStr = "DELETE FROM Annotation " +
-                "WHERE (Label = ?)";
+
+        if (getAnnotationType(label).equals("DropDown")) {
+            removeChoices(label);
+        }
+
+        String statementStr = "DELETE FROM Annotation " + "WHERE (Label = ?)";
+        PreparedStatement deleteAnnotation = conn
+                .prepareStatement(statementStr);
+        deleteAnnotation.setString(1, label);
+        return deleteAnnotation.executeUpdate();
+    }
+
+    private int removeChoices(String label) throws SQLException {
+        String statementStr = "DELETE FROM Annotation_Choices " + "WHERE (Label = ?)";
         PreparedStatement deleteAnnotation = conn.prepareStatement(statementStr);
         deleteAnnotation.setString(1, label);
         return deleteAnnotation.executeUpdate();
+    }
+
+    private String getAnnotationType(String label) throws SQLException {
+        Map<String, String> annotations = getAnnotations();
+        return annotations.get(label);
+    }
+
+    public int addDropDownAnnotation(String label, ArrayList<String> choices)
+            throws SQLException {
+
+        int tuplesInserted = 0;
+
+        String annotationQuery = "INSERT INTO Annotation "
+                + "(Label, DataType) VALUES (?, 'DropDown')";
+        String choicesQuery = "INSERT INTO Annotation_Choices "
+                + "(Label, Value) VALUES (?, ?)";
+        PreparedStatement addAnnotation = null;
+        PreparedStatement addChoices = null;
+
+        addAnnotation = conn.prepareStatement(annotationQuery);
+        addChoices = conn.prepareStatement(choicesQuery);
+        addAnnotation.setString(1, label);
+        tuplesInserted += addAnnotation.executeUpdate();
+
+        addChoices.setString(1, label);
+        for (String choice : choices) {
+            addChoices.setString(2, choice);
+            tuplesInserted += addChoices.executeUpdate();
+        }
+
+        return tuplesInserted;
+
+    }
+
+    public ArrayList<String> getDropDownAnnotations(String label) throws SQLException {
+        String query = "SELECT Value FROM Annotation_Choices " +
+        		"WHERE (Label = ?)";
+        ArrayList<String> dropDownStrings = new ArrayList<String>();
+        PreparedStatement getDropDownStrings = conn.prepareStatement(query);
+        ResultSet rs = getDropDownStrings.executeQuery();
+        while (rs.next()) {
+            dropDownStrings.add(rs.getString("Value"));
+        }
+        return dropDownStrings;
     }
 
 }
