b12c0e3086e53a5d7707e8f61e46526adb08bda6
Christer Jakobsson <dv12cjn@cs.umu.se>
2014-04-30 09:04:54 +0200

    Added ProcessHandler

---
+++ src/process/classes/ProcessHandler.java
@@ -0,0 +1,24 @@
+package process.classes;
+
+import java.io.IOException;
+
+public class ProcessHandler {
+	public ProcessHandler createProcessHandler() {
+		return new ProcessHandler();
+	}
+	//SPRINT 1: Let procedureParams be empty!
+	public String executeProcess(String processName, String[] procedureParams, String inFile, String outFile) throws InterruptedException, IOException {
+		switch(processName){
+		case "rawToProfile":
+			RawToProfileConverter rawToProfileConverter = new RawToProfileConverter();
+
+			rawToProfileConverter.procedure(procedureParams, inFile, outFile);
+			break;
+		case "profileToRegion":
+			// TODOOO
+			break;
+		default: throw new IllegalArgumentException();
+		}
+		return null;
+	}
+}
