10daa5bb5fc1e93d03e48e6b2c35637afd86f4e4
Jonas Engbo <yhi04jeo@cs.umu.se>
2014-04-30 10:28:50 +0200

    lite searchresult

--- src/JUnitTests/SearchResultTest.java
+++ src/JUnitTests/SearchResultTest.java
@@ -7,6 +7,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import org.junit.After;
 import org.junit.Before;
@@ -22,34 +23,41 @@ public class SearchResultTest {
 	private static final String dbUser = "c5dv151_vt14";
 	private static final String dbPass = "shielohh";
 
+	private String query = "SELECT * FROM File NATURAL JOIN Annotated_With " +
+			   "WHERE (Label = ? AND Value = ?)";
+
 	private Connection connection = null;
-	private SearchResult resultPackager = null;
+	private SearchResult searchResult = null;
 
 	@Before
 	public void setup(){
+		PreparedStatement pStatement;
 
 		try{
 			Class.forName(dbDriver);
-		}
-		catch(ClassNotFoundException e) {
+			connection = DriverManager.getConnection(
+					dbConString, dbUser, dbPass);
+			pStatement = connection.prepareStatement(query);
+			pStatement.setString(1, "Species");
+			pStatement.setString(2, "Human");
 
+			ResultSet res = pStatement.executeQuery();
+			searchResult = new SearchResult(res);
+
+			if(res != null){
+				res.close();
+			}
+			pStatement.close();
+		}
+		catch (SQLException e) {
+			System.out.println("Connection Failed! Check output console");
+			e.printStackTrace();
+			return;
+		} catch (ClassNotFoundException e) {
 			System.out.println("Where is your PostgreSQL JDBC Driver? "
 					+ "Include in your library path!");
 			e.printStackTrace();
 			return;
-
-		}
-		try{
-
-			connection = DriverManager.getConnection(
-					dbConString, dbUser, dbPass);
-		}
-		catch (SQLException e) {
-
-			System.out.println("Connection Failed! Check output console");
-			e.printStackTrace();
-			return;
-
 		}
 	}
 
@@ -62,54 +70,93 @@ public class SearchResultTest {
 		}
 	}
 
-
-
 	@Test
-	public void testCreateResultFormat() {
-
-		assertEquals(1,1);
-
-		/*
-		String id = "awe1123";
-		String specie = "Human";
-		String s = "M";
-		String tis = "arm";
-		String cell = "yes";
-		String devS = "Early";
-		String antiN = "cooltName";
-		String antiS = "C";
-		String antiB = "ntzzz";
-
-		String query = "SELECT * FROM  Annotated_With(ExpID, Species, Sex, Tissue," +
-					   " CellType, DevStage, AntiName, Antisymbol, Antibody)"+
-					   " VALUES(?,?,?,?,?,?,?,?,?) RETURNING *";
-
-
-		ResultSet res = null;
-
-		try {
-			PreparedStatement pStatement = connection.prepareStatement(query);
-			pStatement.setString(1, id);
-			pStatement.setString(2, specie);
-			pStatement.setString(3, s);
-			pStatement.setString(4, tis);
-			pStatement.setString(5, cell);
-			pStatement.setString(6, devS);
-			pStatement.setString(7, antiN);
-			pStatement.setString(8, antiS);
-			pStatement.setString(9, antiB);
-
-			res = pStatement.executeQuery();
-
-			if(res != null){
-				res.close();
-			}
-			pStatement.close();
-
-		} catch (SQLException e) {
-			System.out.println("Error! Couldn't insert values,SQLException\n");
-			e.printStackTrace();
-		}*/
+	public void testCreateResult() {
+		assertNotNull(searchResult);
 	}
 
+	@Test
+	public void testColCount() {
+		assertEquals(searchResult.getColCount(), 12);
+	}
+
+	@Test
+	public void testRowCount() {
+		assertEquals(searchResult.getRowCount(), 1);
+	}
+
+	@Test
+	public void testGetList() {
+		assertNotNull(searchResult.getList());
+	}
+
+	@Test
+	public void testSyncHeaderAndValue(){
+		ArrayList<String> headers = searchResult.getColHeaders();
+		ArrayList<String> values = searchResult.getRowValues(0);
+
+		for (int i = 0; i < headers.size(); i++) {
+			//System.out.print(headers.get(i) + ", ");
+		}
+
+		//System.out.println();
+
+		for (int i = 0; i < values.size(); i++) {
+			//System.out.print(values.get(i) + ", ");
+		}
+	}
+
+	@Test
+	public void testGetCorrectValueByAnnotation() {
+		String value = searchResult.getValueByAnnotation(0, "uploader");
+		assertEquals("Per", value);
+	}
+
+	@Test
+	public void testAnnotationIncorrectValues() {
+		String value = searchResult.getValueByAnnotation(0, "ghgh");
+		assertNull(value);
+	}
+
+	@Test
+	public void testAnnotationIncorrectValues2() {
+		String value = searchResult.getValueByAnnotation(-1, "ghgh");
+		assertNull(value);
+	}
+
+	@Test
+	public void testAnnotationIncorrectValues3() {
+		String value = searchResult.getValueByAnnotation(1, "ghgh");
+		assertNull(value);
+	}
+
+	@Test
+	public void testGetCorrectValueByIndex() {
+		String value = searchResult.getValueByIndex(0, 7);
+		assertEquals("Per", value);
+	}
+
+	@Test
+	public void testAnnotationIncorrectIndex() {
+		String value = searchResult.getValueByIndex(-1, 7);
+		assertNull(value);
+	}
+
+	@Test
+	public void testAnnotationIncorrectIndex2() {
+		String value = searchResult.getValueByIndex(9999, 7);
+		assertNull(value);
+	}
+
+	@Test
+	public void testAnnotationIncorrectIndex3() {
+		String value = searchResult.getValueByIndex(9999, -1);
+		assertNull(value);
+	}
+
+	@Test
+	public void testAnnotationIncorrectIndex4() {
+		String value = searchResult.getValueByIndex(9999, 9999);
+		assertNull(value);
+	}
 }
--- src/database/SearchResult.java
+++ src/database/SearchResult.java
@@ -17,44 +17,18 @@ public class SearchResult {
 	private ArrayList<HashMap<String, String>> resultList; //List of Row data
 	private int colCount = 0; //Nr of data columns
 	private int rowCount = 0; //Nr of rows (results)
-	private ArrayList<String> nameList = new ArrayList<String>(); //Annotations
-	private ResultSetMetaData colNames;
+	private ArrayList<String> nameList; //Annotations
 
 	/**
-	 * Inititates a SearchResult object.
-	 */
-	public SearchResult() {
-		resultList = new ArrayList<HashMap<String, String>>();
-	}
-
-	/**
-	 * Adds resultdata. An arraylist of hashmaps will be filled
-	 * with data. This method must be used before getters can return not
-	 * null values. Parameters: ResultSet object
+	 * Inititates a SearchResult object. Parameters: ResultSet object
 	 *
 	 * @param ResultSet r
 	 * @throws SQLException
 	 */
-	public void setResultData(ResultSet r) throws SQLException {
-		colNames = r.getMetaData();
-		colCount = colNames.getColumnCount();
-		HashMap<String, String> rowResult;
+	public SearchResult(ResultSet r) throws SQLException {
+		nameList = new ArrayList<String>();
 
-        while (r.next()) {
-        	rowResult = new HashMap<String, String>();
-
-        	for (int i = 0; i < colCount; i++) {
-        		rowResult.put(colNames.getColumnLabel(i + 1),
-        				r.getString(i + 1));
-        	}
-
-            resultList.add(rowResult);
-        }
-        rowCount = resultList.size();
-
-        for (int i = 1; i <= colCount; i++) {
-			nameList.add(colNames.getColumnName(i));
-		}
+		setResultData(r);
 	}
 
 	/**
@@ -63,6 +37,7 @@ public class SearchResult {
 	 * @return ArrayList<HashMap<String, String>>
 	 */
 	public ArrayList<HashMap<String, String>> getList() {
+
 		return resultList;
 	}
 
@@ -72,6 +47,7 @@ public class SearchResult {
 	 * @return int
 	 */
 	public int getColCount() {
+
 		return colCount;
 	}
 
@@ -81,6 +57,7 @@ public class SearchResult {
 	 * @return int
 	 */
 	public int getRowCount() {
+
 		return rowCount;
 	}
 
@@ -91,6 +68,7 @@ public class SearchResult {
 	 * @return ArrayList<String> or null if failed.
 	 */
 	public ArrayList<String> getRowValues(int rowNr) {
+
 		ArrayList<String> valuesList = new ArrayList<String>();
 
 		if(rowCount > 0){
@@ -105,12 +83,119 @@ public class SearchResult {
 		return null;
 	}
 
-	/**
-	 * Returns an arraylist of annotations; column headers.
-	 *
-	 * @return ArrayList<String>
-	 */
 	public ArrayList<String> getColHeaders() {
+
 		return nameList;
 	}
+
+	/**
+	 * Returns the value of the annotation field on a row. Paramters: row
+	 * number and the annotation. Returns null if table position is incorrect
+	 * or list is empty
+	 *
+	 * @param int rowNr
+	 * @param String annotation
+	 * @return String value
+	 */
+	public String getValueByAnnotation(int rowNr, String annotation) {
+		String value = null;
+
+		if (isWithinBounds(rowNr, resultList.size())) {
+			value = resultList.get(rowNr).get(annotation);
+		}
+
+		return value;
+	}
+
+	/**
+	 * Returns the value of the table by coordinates. Paramters: row
+	 * number and column number. Returns null if table position is incorrect
+	 * or list is empty
+	 *
+	 * @param int rowNr
+	 * @param int colNr
+	 * @return String value
+	 */
+	public String getValueByIndex(int rowNr, int colNr) {
+		String value = null;
+		String annotation = null;
+
+		if (resultList != null) {
+			if (isWithinBounds(rowNr, resultList.size())) {
+				annotation = nameList.get(colNr);
+				value = resultList.get(rowNr).get(annotation);
+			}
+		}
+
+		return value;
+	}
+
+	/**
+	 * Adds resultdata. An arraylist of hashmaps will be filled
+	 * with data. This method must be used before getters can return not
+	 * null values. Parameters: ResultSet object
+	 *
+	 * @param ResultSet r
+	 * @throws SQLException
+	 */
+	private void setResultData(ResultSet r) throws SQLException {
+
+		ResultSetMetaData colNames = r.getMetaData();
+		colCount = colNames.getColumnCount();
+
+		resultList = fillList(r, colNames);
+        rowCount = resultList.size();
+
+        for (int i = 1; i <= colCount; i++) {
+			nameList.add(colNames.getColumnName(i));
+		}
+	}
+
+	/**
+	 * Fills an arrayList with hashmaps with annotations as keys and valus.
+	 *
+	 * @param ResultSet r
+	 * @param ResultSetMetaData colNames
+	 * @return ArrayList<HashMap<String, String>>
+	 * @throws SQLException
+	 */
+	private ArrayList<HashMap<String, String>> fillList(ResultSet r,
+			ResultSetMetaData colNames) throws SQLException {
+
+		HashMap<String, String> rowResult;
+		ArrayList<HashMap<String, String>> list =
+				new ArrayList<HashMap<String, String>>();
+
+        while (r.next()) {
+        	rowResult = new HashMap<String, String>();
+
+        	for (int i = 0; i < colCount; i++) {
+        		rowResult.put(colNames.getColumnLabel(i + 1),
+        				r.getString(i + 1));
+        	}
+
+            list.add(rowResult);
+        }
+
+        return list;
+	}
+
+	/**
+	 * Method for checking that an index is within bounds of the list.
+	 *
+	 * @param index
+	 * @param size
+	 * @return
+	 */
+	private boolean isWithinBounds(int index, int size) {
+
+		boolean isWithin = false;
+
+		if (rowCount > 0 && index >= 0 && index < size) {
+			isWithin = true;
+		}
+
+		return isWithin;
+	}
+
 }
