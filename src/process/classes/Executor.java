436b3f6f57be4fa87deb0e195715e32bed222822
shinowa <shinowa@shinowa-LIFEBOOK-AH532-G21>
2014-05-07 14:17:07 +0200

    Refactored

--- src/process/classes/Executor.java
+++ src/process/classes/Executor.java
@@ -1,28 +1,56 @@
 package process.classes;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 
+/**
+ * Class that is abstract and contains methods that all analysis needs to use.
+ *
+ */
 public abstract class Executor {
 
 private final String FILEPATH = "resources/";
-
+	
+	/**
+	 * Used to execute a program like bowtie
+	 * 
+	 * @param command
+	 * @return
+	 * @throws InterruptedException
+	 * @throws IOException
+	 */
 	protected String executeProgram(String[] command) throws InterruptedException, IOException{
 
 		File pathToExecutable = new File( FILEPATH + command[0] );
 		command[0]=pathToExecutable.getAbsolutePath();
 		return executeCommand(command);
 	}
-
+	
+	/**
+	 * Used to execute a script
+	 * 
+	 * @param command
+	 * @return
+	 * @throws InterruptedException
+	 * @throws IOException
+	 */
 	protected String executeScript(String[] command) throws InterruptedException, IOException{
 
 		File pathToExecutable = new File( FILEPATH + command[1] );
 		command[1]=pathToExecutable.getAbsolutePath();
 		return executeCommand(command);
 	}
-
+	
+	/**
+	 * Used to parse a string and make it into a String array
+	 * 
+	 * @param procedureParameters
+	 * @return
+	 */
 	protected String[] parse(String procedureParameters){
 		StringTokenizer paramTokenizer = new StringTokenizer(procedureParameters);
 		String[] temp = new String[paramTokenizer.countTokens()];
@@ -33,7 +61,15 @@ private final String FILEPATH = "resources/";
 		}
 		return temp;
 	}
-
+	
+	/**
+	 * Used to execute commands
+	 * 
+	 * @param command
+	 * @return
+	 * @throws InterruptedException
+	 * @throws IOException
+	 */
 	private String executeCommand(String[] command) throws InterruptedException, IOException{
 		ProcessBuilder builder = new ProcessBuilder(command);
 
@@ -53,6 +89,77 @@ private final String FILEPATH = "resources/";
 
 //		System.out.printf( "Process exited with result %d and output %s%n", result, text );
 		return text.toString();
+	}
+
+	/**
+	 * Used to execute shell command
+	 * 
+	 * @param command
+	 * @param dir
+	 * @param fileName
+	 * @return
+	 * @throws InterruptedException
+	 * @throws IOException
+	 */
+	protected void executeShellCommand(String[] command, String dir, String fileName) throws InterruptedException, IOException{
+		ProcessBuilder builder = new ProcessBuilder(command);
+
+		builder.directory( new File( FILEPATH ).getAbsoluteFile() );
+		builder.redirectErrorStream(true);
+		Process process =  builder.start();
+
+		Scanner s = new Scanner(process.getInputStream());
+		StringBuilder text = new StringBuilder();
+		File dirFile = new File(FILEPATH+dir);
+
+		if(!dirFile.exists()) {
+			dirFile.mkdirs();
+		}
+		File file = new File(dirFile.toString()+"/"+fileName);
+		FileWriter fw = new FileWriter(file.getAbsoluteFile());
+		BufferedWriter bw = new BufferedWriter(fw);
+		while (s.hasNextLine()) {
+			bw.append(s.nextLine());
+			bw.append("\n");
+		}
+		bw.close();
+		s.close();
+
+		int result = process.waitFor();
+
+//		System.out.printf( "Process exited with result %d and output %s%n", result, text );
+		return text.toString();
+	}
+	
+	/**
+	 * Used to make a File object which represents a folder.
+	 * 
+	 * @param dirName
+	 * @return
+	 */
+	protected File cleanUpInitiator(String dirName) {
+
+		return new File(dirName.substring(0, dirName.length()-1));
+
+	}
+	
+	/**
+	 * Deletes a folder and all its subfolders.
+	 * 
+	 * @param dir
+	 * @return
+	 */
+	protected static boolean cleanUp(File dir) {
+		if (dir.isDirectory()) {
+			String[] children = dir.list();
+			for (int i = 0; i < children.length; i++) {
+				boolean success = cleanUp(new File(dir, children[i]));
+				if (!success) {
+					return false;
+				}
+			}
+		}
+		return dir.delete();
 	}
 
 }
--- src/process/classes/RawToProfileConverter.java
+++ src/process/classes/RawToProfileConverter.java
@@ -1,42 +1,192 @@
 package process.classes;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
+/**
+ * Class used to create .wig file from .fastq format.
+ * 
+ * @version 1.0
+ */
 public class RawToProfileConverter extends Executor {
 
-		private String[] bowTieParameters;
-		private String[] samToBamParameters;
-		private String[] sortBamParameters = new String[]{"samtools-0.1.19/samtools", "sort", "test/male.bam", "test/maleSorted"};
-		private String readgff = "perl sam_to_readsgff_v1.pl test/hej/";
+	private String[] bowTieParameters; // Step 1
+	private final String SMOOTH = "perl smooth_v4.pl ";
+	private final String GFF_TO_ALLNUCSGR = "perl readsgff_to_allnucsgr_v1.pl ";
+	private final String SGR_2_WIG = "perl sgr2wig.pl ";
+	private final String SAM_TO_GFF = "perl sam_to_readsgff_v1.pl ";
+	private final String STEP10 = "perl AllSeqRegSGRtoPositionSGR_v1.pl y 10 ";
+	private final String RATIO_CALC = "perl ratio_calculator_v2 ";
+	/**
+	 * Takes a string array that contains bowtie parameters and then follows a
+	 * procedure.
+	 * 
+	 * 1. runs the bowtie program to get a .sam file. 2. runs a linux shell
+	 * command to sort the sam file. 3. runs a perl script that creates a .ggf
+	 * file from the sam file. 4. runs a perl script that creates a .sgr file
+	 * from the .gff file. 5. runs a perl script to smooth the .sgr file. 6.
+	 * runs a perl script to convert the .sgr file to a .sgr with fixed size. 7.
+	 * runs a perl script that converts the .sgr file to .wig file.
+	 * 
+	 * 
+	 * 
+	 * @param parameters
+	 *            String array with execution parameters
+	 * @param inFile
+	 *            The filepath to the file to create a wig from
+	 * @param outFile
+	 *            Filepath to where the .wig file should be placed.
+	 * @throws InterruptedException
+	 * @throws IOException
+	 */
+	public void procedure(String[] parameters, String inFolder, String outFile, boolean ratioCalc)
+			throws InterruptedException, IOException {
+		File[] inFiles = new File("resources/" + inFolder).getAbsoluteFile()
+				.listFiles();
+		String fileOne;
+		String fileTwo;
+		String fileOneName;
+		String fileTwoName;
 
-		public void procedure(String[] parameters, String inFile, String outFile) throws InterruptedException, IOException {
-			bowTieParameters = parse(parameters[0]);
-			samToBamParameters = new String[]{"samtools-0.1.19/samtools", "view", "-bS", "-o", "test/male.bam", "test/male.sam"};
+		fileOne = inFiles[0].getName();
+		fileTwo = inFiles[1].getName();
+		fileOneName = fileOne.substring(0, fileOne.length() - 6);
+		fileTwoName = fileTwo.substring(0, fileTwo.length() - 6);
 
-			executeProgram(bowTieParameters);
-			System.out.println("hej");
-			for(int i=0; i<samToBamParameters.length; i++){
-				System.out.println(samToBamParameters[i]+ " ");
+		String dir = "results_" + Thread.currentThread().getId() + "/";
+		String sortedDir = dir + "sorted/";
 
-			}
-			executeScript(parse(readgff));
-			System.out.println("Efter readgff");
+		String outString = null;
 
-//			executeProgram(samToBamParameters);
-//			executeProgram(sortBamParameters);
-//			String temp = executeProgram(parse(test));
-			File bamTestFile = new File("resources/test/bamTest");
-			if(!bamTestFile.exists()) {
-				bamTestFile.createNewFile();
-			}
-			System.out.println('"' + "då"+'"');
+		File fileDir = new File("resources/" + dir);
+		// deleteDir(fileDir);
+		System.out.println("Deletes directory");
+
+		System.out.println("dir " + fileDir.toString());
+		if (!fileDir.exists()) {
+			fileDir.mkdir();
 		}
 
-		public String[] getBowTieParameters() {
-			return bowTieParameters;
+		if (fileDir.exists()) {
+
+			String sortSam; // Step 2
+			
+			// Bowtie on 2 files
+			outString = runBowTie(parameters, inFolder, fileOne, fileTwo,
+					fileOneName, fileTwoName, dir, outString);
+
+			//Sort sam file one
+			sortSam = "sort " + dir + fileOneName + ".sam" + " -k 3,3 -k 4,4n";
+			doSortSam(fileOneName, dir, sortSam);
+
+			//Sort sam file two
+			sortSam = "sort " + dir + fileTwoName + ".sam" + " -k 3,3 -k 4,4n";
+			doSortSam(fileTwoName, dir, sortSam);
+																
+			
+			outString = outString + " " + executeScript(parse(SAM_TO_GFF+sortedDir));
+
+			outString = outString + " " + executeScript(parse(GFF_TO_ALLNUCSGR+sortedDir + "reads_gff/"));
+
+			outString = outString + " " + executeScript(parse(SMOOTH+sortedDir
+					+ "reads_gff/allnucs_sgr/ "+"10 1 5 0 0"));
+
+			outString = outString + " " + executeScript(parse(STEP10+sortedDir + "reads_gff/allnucs_sgr/smoothed/"));
+
+			File[] sgrFiles = new File("resources/" + sortedDir
+					+ "/reads_gff/allnucs_sgr/smoothed/Step10")
+					.getAbsoluteFile().listFiles();
+
+//			if(ratioCalc) {
+//				ratioCalc("resources/" + sortedDir
+//						+ "/reads_gff/allnucs_sgr/smoothed/Step10/");
+//			} else {
+			
+			fileOne = sgrFiles[0].getName();
+			fileTwo = sgrFiles[1].getName();
+			fileOneName = fileOne.substring(0, fileOne.length() - 6);
+			fileTwoName = fileTwo.substring(0, fileTwo.length() - 6);
+
+			outString = doSgr2Wig(outFile, fileOne, fileTwo, fileOneName,
+					fileTwoName, sortedDir, outString);
+//			}
+			
+			System.out.println("cleanup gav tillbaka = "
+					+ cleanUp(cleanUpInitiator("resources/" + dir)));
+
+			System.out.println(outString);
 		}
+	}
 
+//	private void ratioCalc(String dir) throws InterruptedException, IOException {
+//		executeScript(parse(RATIO_CALC+dir
+//				+ "reads_gff/allnucs_sgr/smoothed/step10/ "+"4 0"));
+//		
+//		executeScript(SMOOTH+"reads_gff/allnucs_sgr/smoothed/step10/");
+//	}
 
+	private String doSgr2Wig(String outFile, String fileOne, String fileTwo,
+			String fileOneName, String fileTwoName, String sortedDir,
+			String outString) throws InterruptedException, IOException {
+		
+		outString = outString + " " + executeScript(parse(SGR_2_WIG+sortedDir
+				+ "/reads_gff/allnucs_sgr/smoothed/Step10/" + fileOne + " "
+				+ outFile + fileOneName + ".wig"));
 
+		outString = outString + " " + executeScript(parse(SGR_2_WIG+sortedDir
+				+ "/reads_gff/allnucs_sgr/smoothed/Step10/" + fileTwo + " "
+				+ outFile + "/" + fileTwoName + ".wig"));
+
+		return outString;
+	}
+
+	private String runBowTie(String[] parameters, String inFolder,
+			String fileOne, String fileTwo, String fileOneName,
+			String fileTwoName, String dir, String outString)
+			throws InterruptedException, IOException {
+		bowTieParameters = parse(parameters[0] + " " + inFolder + "/"
+				+ fileOne + " " + dir + fileOneName + ".sam");
+		outString = outString + " " + doBowtie(bowTieParameters);
+
+		// Bowtie on fileTwo
+		bowTieParameters = parse(parameters[0] + " " + inFolder + "/"
+				+ fileTwo + " " + dir + fileTwoName + ".sam");
+		outString = outString + " " + doBowtie(bowTieParameters);
+		return outString;
+	}
+
+	private void doSortSam(String fileOneName, String dir, String sortSam)
+			throws InterruptedException, IOException {
+		executeShellCommand(parse(sortSam), dir + "sorted/", fileOneName
+				+ "_sorted.sam");
+	}
+
+	private String doBowtie(String[] strings) throws InterruptedException,
+			IOException {
+		return executeProgram(bowTieParameters);
+	}
+
+	public String getTime(long temp) {
+		String out = "";
+
+		String minute;
+		String second;
+
+		second = String.valueOf((temp % 60000) / 1000);
+		minute = String.valueOf(temp / 60000);
+
+		if (second.length() == 1) {
+			out = minute + ":0" + second;
+		} else {
+			out = minute + ":" + second;
+		}
+		System.out.println(out);
+		return out;
+	}
+
+	public String[] getBowTieParameters() {
+		return bowTieParameters;
+	}
 }
