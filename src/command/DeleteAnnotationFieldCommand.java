b8a10c85817c25fd75bd6e45b18f2f56849287e9
tfy09jnn <tfy09jnn@outlook.com>
2014-05-07 16:12:49 +0200

    Added some code to deleteAnnotations.

--- src/command/DeleteAnnotationFieldCommand.java
+++ src/command/DeleteAnnotationFieldCommand.java
@@ -1,8 +1,15 @@
 package command;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.google.gson.annotations.Expose;
+
+import database.DatabaseAccessor;
 import response.MinimalResponse;
 import response.Response;
 import response.StatusCode;
+import server.DatabaseSettings;
 
 /**
  * Class used to represent a logout command.
@@ -11,6 +18,20 @@ import response.StatusCode;
  * @version 1.0
  */
 public class DeleteAnnotationFieldCommand extends Command {
+
+
+	/*
+{
+ "deleteId": [
+              { "id": 1, "values": ["man", "mouse"] },
+              { "id": 2, "values": [ ] },
+              { "id": 3, "values": ["leg"] }
+             ]
+}
+*/
+
+	@Expose
+	private ArrayList<String> deleteId = new ArrayList<String>();
 
 	/**
 	 * Used to validate the logout command.
@@ -28,6 +49,36 @@ public class DeleteAnnotationFieldCommand extends Command {
 	 */
 	@Override
 	public Response execute() {
+
+		Response rsp;
+		int result = 0;
+
+		try {
+
+			DatabaseAccessor dbAccess = new DatabaseAccessor(DatabaseSettings.username, DatabaseSettings.password, DatabaseSettings.host, DatabaseSettings.database);
+
+
+			//TODO: Add the label. API looks wierd currently.
+			//result = dbAccess.deleteAnnotation(label);
+
+			if(result == 0) {
+
+				rsp = new MinimalResponse(403);
+
+			} else {
+
+				rsp = new MinimalResponse(200);
+
+			}
+
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+
+
+
 
 		//Method not implemented, send appropriate response
 		return 	new MinimalResponse(StatusCode.NO_CONTENT);
