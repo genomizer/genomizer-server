b537abea5e760c9958d4d4e7bbf84bb7cd7d8547
c11jmm <c11jmm@ceylon.cs.umu.se>
2014-05-07 14:02:54 +0200

    fixed annotations

--- src/response/AnnotationInformation.java
+++ src/response/AnnotationInformation.java
@@ -6,8 +6,6 @@ import com.google.gson.annotations.Expose;
 
 public class AnnotationInformation {
 
-	public static final int TYPE_DROP_DOWN = 1;
-	public static final int TYPE_FREE_TEXT = 2;
 
 	@Expose
 	private int id;
@@ -21,18 +19,13 @@ public class AnnotationInformation {
 	@Expose
 	private boolean forced;
 
-	public AnnotationInformation(int id, String name, int type,
+	public AnnotationInformation(int id, String name,
 			ArrayList<String> values, boolean forced) {
 
 		this.id = id;
 		this.name = name;
 		this.values = values;
 		this.forced = forced;
-
-		if(type == TYPE_FREE_TEXT) {
-			values.clear();
-			values.add("freetext");
-		}
 	}
 
 	public int getId() {
