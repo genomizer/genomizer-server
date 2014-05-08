fe024c38bef2559cbecb854bf7a12d966d626f27
Ruaridh Watt <ruaridh.watt@gmail.com>
2014-05-08 13:37:52 +0200

    New ways to add files being implemented

--- src/database/FileTuple.java
+++ src/database/FileTuple.java
@@ -6,10 +6,16 @@ import java.util.Date;
 
 public class FileTuple {
 
+    public static final int RAW = 1;
+    public static final int PROFILE = 2;
+    public static final int REGION = 3;
+    public static final int OTHER = 4;
+
     public final Integer id;
     public final String path;
+    public final String inputFilePath;
     public final String DownloadURL;
-    public final String type;
+    public final int type;
     public final String filename;
     public final Date date;
     public final String metaData;
@@ -22,8 +28,24 @@ public class FileTuple {
     public FileTuple(ResultSet resSet) throws SQLException {
         id = resSet.getInt("FileID");
         path = resSet.getString("Path");
+        inputFilePath = resSet.getString("InputFilePath");
         DownloadURL = ServerDependentValues.DownLoadURL+path;
-        type = resSet.getString("FileType");
+
+        switch (resSet.getString("FileType")) {
+        case "Raw":
+            type = RAW;
+            break;
+        case "Profile":
+            type = PROFILE;
+            break;
+        case "Region":
+            type = REGION;
+            break;
+        default:
+            type = OTHER;
+            break;
+        }
+
         filename = resSet.getString("FileName");
         date = resSet.getDate("Date");
         metaData = resSet.getString("MetaData");
@@ -33,5 +55,4 @@ public class FileTuple {
         expId = resSet.getString("ExpID");
         grVersion = resSet.getString("GRVersion");
     }
-
 }
