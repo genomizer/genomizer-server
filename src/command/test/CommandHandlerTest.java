6f8946e0984f62cc10b5bf122ad63a3d12d98307
tfy09jnn <tfy09jnn@outlook.com>
2014-05-08 16:03:23 +0200

    Added creation JUnit testcase to CommandHandlerTest

--- src/command/test/CommandHandlerTest.java
+++ src/command/test/CommandHandlerTest.java
@@ -2,13 +2,28 @@ package command.test;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-
 import org.junit.Test;
 
 import command.CommandHandler;
 
+/**
+ * Testclass used to test the CommandHandler class.
+ *
+ * @author tfy09jnn
+ * @version 1.0
+ */
 public class CommandHandlerTest {
+
+	/**
+	 * Test method used to test creation of the class.
+	 */
+	@Test
+	public void testCreateClass() {
+
+		CommandHandler cmdh = new CommandHandler();
+		assertNotNull(cmdh);
+
+	}
 
 	@Test
 	public void testProcessNewCommand() {
@@ -17,11 +32,11 @@ public class CommandHandlerTest {
 
 	@Test
 	public void testParseRest() {
-		
+
 		CommandHandler handler = new CommandHandler();
 		String rest = handler.parseRest("/search/annotations=abc123");
 		assertEquals("annotations=abc123", rest);
-		
+
 		rest = handler.parseRest("/file/expId=2/fileId=1");
 		assertEquals("fileId=1", rest);
 
