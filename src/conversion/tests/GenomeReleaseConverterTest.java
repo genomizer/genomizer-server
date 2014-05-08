adcfa6bad1246affb8a50571daba8f45d08eedc1
David Hansson <c11dhn@cs.umu.se>
2014-05-08 13:00:17 +0200

    profileDataConverter uppdated

--- src/conversion/tests/GenomeReleaseConverterTest.java
+++ src/conversion/tests/GenomeReleaseConverterTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import conversion.classes.ConversionHandler;
 import conversion.classes.GenomeReleaseConverter;
 
 public class GenomeReleaseConverterTest {
@@ -14,6 +16,11 @@ public class GenomeReleaseConverterTest {
 	public void test() {
 		GenomeReleaseConverter handler = new GenomeReleaseConverter();
 		handler.procedure("test.bed", "temp.bed", "dm2ToDm3.over.chain.gz", "unlifted.bed");
+	}
+	@Test
+	public void testSgrToBedConversion() {
+		ConversionHandler ch = new ConversionHandler();
+
 	}
 
 }
