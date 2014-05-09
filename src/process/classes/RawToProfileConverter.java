3e6ed93fc66d7383c375de94e045e21a73220b4a
Christer Jakobsson <dv12cjn@cs.umu.se>
2014-05-09 15:53:00 +0200

    added comments, cleaned code

--- src/process/classes/RawToProfileConverter.java
+++ src/process/classes/RawToProfileConverter.java
@@ -6,32 +6,41 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
- * Class used to create .wig file from .fastq format.
- * 
+ * Class used to create profile data from .fastq format.
+ *
  * @version 1.0
  */
 public class RawToProfileConverter extends Executor {
 
-	private String[] bowTieParameters; // Step 1
-	private final String SMOOTH = "perl smooth_v4.pl ";
-	private final String GFF_TO_ALLNUCSGR = "perl readsgff_to_allnucsgr_v1.pl ";
-	private final String SGR_2_WIG = "perl sgr2wig.pl ";
-	private final String SAM_TO_GFF = "perl sam_to_readsgff_v1.pl ";
-	private final String STEP10 = "perl AllSeqRegSGRtoPositionSGR_v1.pl y 10 ";
-	private final String RATIO_CALC = "perl ratio_calculator_v2 ";
+	private String remoteExecution;
+	private String dir;
+	private String sortedDir;
+	private String samToGff;
+	private String gffToAllnusgr;
+	private String smooth;
+	private String step10;
+	private File fileDir;
+	private String inFolder;
+	private String[] parameters;
+	private String rawFile1;
+	private String rawFile2;
+	private String rawFile_1_Name;
+	private String rawFile_2_Name;
+	private String logString;
+
 	/**
 	 * Takes a string array that contains bowtie parameters and then follows a
 	 * procedure.
-	 * 
+	 *
 	 * 1. runs the bowtie program to get a .sam file. 2. runs a linux shell
 	 * command to sort the sam file. 3. runs a perl script that creates a .ggf
 	 * file from the sam file. 4. runs a perl script that creates a .sgr file
 	 * from the .gff file. 5. runs a perl script to smooth the .sgr file. 6.
 	 * runs a perl script to convert the .sgr file to a .sgr with fixed size. 7.
 	 * runs a perl script that converts the .sgr file to .wig file.
-	 * 
-	 * 
-	 * 
+	 *
+	 *
+	 *
 	 * @param parameters
 	 *            String array with execution parameters
 	 * @param inFile
@@ -41,152 +50,203 @@ public class RawToProfileConverter extends Executor {
 	 * @throws InterruptedException
 	 * @throws IOException
 	 */
-	public void procedure(String[] parameters, String inFolder, String outFile, boolean ratioCalc)
-			throws InterruptedException, IOException {
-		File[] inFiles = new File("resources/" + inFolder).getAbsoluteFile()
-				.listFiles();
-		String fileOne;
-		String fileTwo;
-		String fileOneName;
-		String fileTwoName;
+	public String procedure(String[] parameters, String inFolder,
+			String outFilePath) throws InterruptedException, IOException {
+		File[] inFiles = new File("/" + inFolder).listFiles();
+		this.parameters = parameters;
+		this.inFolder = inFolder;
 
-		fileOne = inFiles[0].getName();
-		fileTwo = inFiles[1].getName();
-		fileOneName = fileOne.substring(0, fileOne.length() - 6);
-		fileTwoName = fileTwo.substring(0, fileTwo.length() - 6);
+		if (inFiles.length == 0) {
+			return "No files found at path " + inFolder;
+		} else if (inFiles.length > 2) {
+			return "Experiment contains more than two raw files";
+		}
 
-		String dir = "results_" + Thread.currentThread().getId() + "/";
-		String sortedDir = dir + "sorted/";
+		rawFile1 = inFiles[0].getName();
+		rawFile_1_Name = rawFile1.substring(0, rawFile1.length() - 6);
 
-		String outString = null;
+		if (inFiles.length == 2) {
+			rawFile2 = inFiles[1].getName();
+			rawFile_2_Name = rawFile2.substring(0, rawFile2.length() - 6);
+		}
 
-		File fileDir = new File("resources/" + dir);
-		// deleteDir(fileDir);
+		initiateConversionStrings(parameters, outFilePath);
 		System.out.println("Deletes directory");
 
-		System.out.println("dir " + fileDir.toString());
-		if (!fileDir.exists()) {
-			fileDir.mkdir();
-		}
+		makeConversionDirectories(remoteExecution + dir + "/sorted");
+
+		printTrace(parameters, inFolder, outFilePath);
 
 		if (fileDir.exists()) {
+			logString = runBowTie(rawFile1, rawFile_1_Name);
+			sortSamFile(rawFile_1_Name);
+			if (inFiles.length == 2) {
+				logString = logString + "\n"
+						+ runBowTie(rawFile2, rawFile_2_Name);
+				// Sets parameters for sorting second sam file
+				sortSamFile(rawFile_2_Name);
+			}// Sets parameters for sorting first sam file
+			logString = logString + "\n" + executeScript(parse(samToGff));
+			logString = logString + "\n" + executeScript(parse(gffToAllnusgr));
+			logString = logString + "\n" + executeScript(parse(smooth));
+			logString = logString + "\n" + executeScript(parse(step10));
 
-			String sortSam; // Step 2
-			
-			// Bowtie on 2 files
-			outString = runBowTie(parameters, inFolder, fileOne, fileTwo,
-					fileOneName, fileTwoName, dir, outString);
+			if (parameters.length == 4) {
+				moveEndFiles(sortedDir
+						+ "reads_gff/allnucs_sgr/smoothed/Step10/", outFilePath);
+			} else {
+				doRatioCalculation(sortedDir
+						+ "reads_gff/allnucs_sgr/smoothed/Step10/", parameters);
+				moveEndFiles(
+						sortedDir
+								+ "reads_gff/allnucs_sgr/smoothed/Step10/ratios/smoothed/",
+						outFilePath);
+			}
+			/*
+			 * //Parameters for first sgr2wig execution sgr2wig =
+			 * "perl sgr2wig.pl " + sortedDir +
+			 * "/reads_gff/allnucs_sgr/smoothed/Step10/"+fileOne +" "+
+			 * outFile+fileOneName + ".wig"; // Step 7 outString = outString +
+			 * " " + executeScript(parse(sgr2wig));
+			 */
+			// cleanUp(cleanUpInitiator(remoteExecution+dir));
 
-			//Sort sam file one
-			sortSam = "sort " + dir + fileOneName + ".sam" + " -k 3,3 -k 4,4n";
-			doSortSam(fileOneName, dir, sortSam);
-
-			//Sort sam file two
-			sortSam = "sort " + dir + fileTwoName + ".sam" + " -k 3,3 -k 4,4n";
-			doSortSam(fileTwoName, dir, sortSam);
-																
-			
-			outString = outString + " " + executeScript(parse(SAM_TO_GFF+sortedDir));
-
-			outString = outString + " " + executeScript(parse(GFF_TO_ALLNUCSGR+sortedDir + "reads_gff/"));
-
-			outString = outString + " " + executeScript(parse(SMOOTH+sortedDir
-					+ "reads_gff/allnucs_sgr/ "+"10 1 5 0 0"));
-
-			outString = outString + " " + executeScript(parse(STEP10+sortedDir + "reads_gff/allnucs_sgr/smoothed/"));
-
-			File[] sgrFiles = new File("resources/" + sortedDir
-					+ "/reads_gff/allnucs_sgr/smoothed/Step10")
-					.getAbsoluteFile().listFiles();
-
-//			if(ratioCalc) {
-//				ratioCalc("resources/" + sortedDir
-//						+ "/reads_gff/allnucs_sgr/smoothed/Step10/");
-//			} else {
-			
-			fileOne = sgrFiles[0].getName();
-			fileTwo = sgrFiles[1].getName();
-			fileOneName = fileOne.substring(0, fileOne.length() - 6);
-			fileTwoName = fileTwo.substring(0, fileTwo.length() - 6);
-
-			outString = doSgr2Wig(outFile, fileOne, fileTwo, fileOneName,
-					fileTwoName, sortedDir, outString);
-//			}
-			
-			System.out.println("cleanup gav tillbaka = "
-					+ cleanUp(cleanUpInitiator("resources/" + dir)));
-
-			System.out.println(outString);
+			return logString;
+		} else {
+			return "Failed to create directory " + fileDir.toString();
 		}
 	}
 
-//	private void ratioCalc(String dir) throws InterruptedException, IOException {
-//		executeScript(parse(RATIO_CALC+dir
-//				+ "reads_gff/allnucs_sgr/smoothed/step10/ "+"4 0"));
-//		
-//		executeScript(SMOOTH+"reads_gff/allnucs_sgr/smoothed/step10/");
-//	}
-
-	private String doSgr2Wig(String outFile, String fileOne, String fileTwo,
-			String fileOneName, String fileTwoName, String sortedDir,
-			String outString) throws InterruptedException, IOException {
-		
-		outString = outString + " " + executeScript(parse(SGR_2_WIG+sortedDir
-				+ "/reads_gff/allnucs_sgr/smoothed/Step10/" + fileOne + " "
-				+ outFile + fileOneName + ".wig"));
-
-		outString = outString + " " + executeScript(parse(SGR_2_WIG+sortedDir
-				+ "/reads_gff/allnucs_sgr/smoothed/Step10/" + fileTwo + " "
-				+ outFile + "/" + fileTwoName + ".wig"));
-
-		return outString;
-	}
-
-	private String runBowTie(String[] parameters, String inFolder,
-			String fileOne, String fileTwo, String fileOneName,
-			String fileTwoName, String dir, String outString)
+	/**
+	 * Initiates strings using the incoming parameters and executes the two
+	 * scripts to do the ratio calculation.
+	 *
+	 * @param dirPath
+	 *            the path to where the files used to run ratio calculation is
+	 * @param parameters
+	 *            contains the parameters for the two scripts
+	 * @throws InterruptedException
+	 * @throws IOException
+	 */
+	private void doRatioCalculation(String dirPath, String[] parameters)
 			throws InterruptedException, IOException {
-		bowTieParameters = parse(parameters[0] + " " + inFolder + "/"
-				+ fileOne + " " + dir + fileOneName + ".sam");
-		outString = outString + " " + doBowtie(bowTieParameters);
+		String ratioCalc = "perl ratio_calculator_v2.pl " + dirPath + " "
+				+ parameters[4];
+		String smooth = "perl smooth_v4.pl " + dirPath + "ratios/" + " "
+				+ parameters[5];
+		executeScript(parse(ratioCalc));
+		executeScript(parse(smooth));
 
-		// Bowtie on fileTwo
-		bowTieParameters = parse(parameters[0] + " " + inFolder + "/"
-				+ fileTwo + " " + dir + fileTwoName + ".sam");
-		outString = outString + " " + doBowtie(bowTieParameters);
-		return outString;
 	}
 
-	private void doSortSam(String fileOneName, String dir, String sortSam)
+	/**
+	 * Prints important variables, used for testing.
+	 *
+	 * @param parameters
+	 * @param inFolder
+	 * @param outFile
+	 */
+	private void printTrace(String[] parameters, String inFolder, String outFile) {
+		System.out.println("dir " + fileDir.toString());
+		System.out.println("INFOLDER = " + inFolder);
+		System.out.println("OUTFILE = " + outFile);
+		System.out.println("DIR = " + dir);
+		System.out.println("SORTEDDIR = " + sortedDir);
+		System.out.println("BOWTIE = "
+				+ parse(parameters[0] + " " + inFolder + "/" + rawFile1 + " "
+						+ dir + rawFile_1_Name + ".sam"));
+	}
+
+	/**
+	 * Constructs a string array with the values to run bowtie on the file that
+	 * comes as parameter.
+	 *
+	 * @param fileOne
+	 *            the name of the file with the file extension.
+	 * @param fileOneName
+	 *            the name of the file without the file extension.
+	 * @return the value that bowtie returns.
+	 * @throws InterruptedException
+	 * @throws IOException
+	 */
+	private String runBowTie(String fileOne, String fileOneName)
 			throws InterruptedException, IOException {
-		executeShellCommand(parse(sortSam), dir + "sorted/", fileOneName
-				+ "_sorted.sam");
-	}
+		String[] bowTieParameters = parse("bowtie " + parameters[0] + " "
+				+ parameters[1] + " " + inFolder + "/" + fileOne + " "
+				+ remoteExecution + dir + fileOneName + ".sam");
 
-	private String doBowtie(String[] strings) throws InterruptedException,
-			IOException {
+		printStringArray(bowTieParameters);
 		return executeProgram(bowTieParameters);
 	}
 
-	public String getTime(long temp) {
-		String out = "";
-
-		String minute;
-		String second;
-
-		second = String.valueOf((temp % 60000) / 1000);
-		minute = String.valueOf(temp / 60000);
-
-		if (second.length() == 1) {
-			out = minute + ":0" + second;
-		} else {
-			out = minute + ":" + second;
+	/**
+	 * Prints a string array, used for testing.
+	 *
+	 * @param s
+	 * @return
+	 */
+	private String printStringArray(String[] s) {
+		String string = "";
+		for (int i = 0; i < s.length; i++) {
+			string += s[i] + " ";
 		}
-		System.out.println(out);
-		return out;
+		System.out.println(string);
+		return string;
 	}
 
-	public String[] getBowTieParameters() {
-		return bowTieParameters;
+	/**
+	 * Constructs a string with values to run a linux command that sorts a file
+	 * with the specified parameters. puts a new sorted fil in a specified path.
+	 *
+	 * @param unsortedSamFileName
+	 *            the name of the unsorted sam file
+	 * @throws InterruptedException
+	 * @throws IOException
+	 */
+	private void sortSamFile(String unsortedSamFileName)
+			throws InterruptedException, IOException {
+		String sortSam = "sort " + remoteExecution + dir + unsortedSamFileName
+				+ ".sam" + " -k 3,3 -k 4,4n";
+		executeShellCommand(parse(sortSam), remoteExecution + dir + "sorted/",
+				unsortedSamFileName + "_sorted.sam");
+
+	}
+
+	/**
+	 * Creates the working directory for the procedure to put its files in.
+	 *
+	 *
+	 * @param directoryPath
+	 *            the directory to create if it doesnt exist
+	 */
+	private void makeConversionDirectories(String directoryPath) {
+		fileDir = new File(directoryPath);
+		if (!fileDir.exists()) {
+			fileDir.mkdirs();
+		}
+
+	}
+
+	/**
+	 * Initiates strings that is used to run programs and scripts and also
+	 * strings that specifies directories
+	 *
+	 * @param parameters
+	 * @param outFile
+	 */
+	private void initiateConversionStrings(String[] parameters, String outFile) {
+		remoteExecution = "/scratch/resources/";
+		dir = "results_" + Thread.currentThread().getId() + "/";
+		sortedDir = remoteExecution + dir + "sorted/";
+		samToGff = "perl sam_to_readsgff_v1.pl " + sortedDir;
+		gffToAllnusgr = "perl readsgff_to_allnucsgr_v1.pl " + sortedDir
+				+ "reads_gff/";
+		smooth = "perl smooth_v4.pl " + sortedDir + "reads_gff/allnucs_sgr/ "
+				+ parameters[2]; // Step 5
+		step10 = "perl AllSeqRegSGRtoPositionSGR_v1.pl" + parameters[3] + " "
+				+ sortedDir + "reads_gff/allnucs_sgr/smoothed/"; // Step 6
+		// sgr2wig = "perl sgr2wig.pl " + sortedDir
+		// + "/reads_gff/allnucs_sgr/smoothed/Step10/*.sgr " + outFile
+		// + "test.wig";
 	}
 }
