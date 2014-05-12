7bbeb51e11405cf54660a17446286c845aae0f91
David Hansson <c11dhn@cs.umu.se>
2014-05-12 12:57:53 +0200

    Conversion uppdate

--- src/conversion/classes/ConversionHandler.java
+++ src/conversion/classes/ConversionHandler.java
@@ -1,118 +1,209 @@
 package conversion.classes;
 
 import java.io.File;
-
+import java.io.IOException;
 
 public class ConversionHandler {
 	public ConversionHandler createConversionHandler() {
 		return new ConversionHandler();
 	}
 
+	/**
+	 * * @param secondColumn -- IMPORTANT, null unless converting to bed
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	public String executeProfileDataConversion(String conversionName,
+			String inFile, String outFile, String secondColumn) throws InterruptedException, IOException {
 
-	public String executeProfileDataConversion(String conversionName, String inFile){
-		switch(conversionName){
-		case "wigToSgr":
-			break;
-		case "wigToBed":
-			break;
-		case "wigToGff3":
-			break;
-		case "bedToWig":
-			break;
-		case "bedToSgr":
-			break;
-		case "bedToGff3":
-			break;
-		case "sgrToWig":
-			break;
-		case "sgrToBed":
-			break;
-		case "sgrToGff3":
-			break;
-		case "gff3ToBed":
-			break;
-		case "gff3ToSgr":
-			break;
-		case "gff3ToWig":
-			break;
-		default: throw new IllegalArgumentException();
-		}
-		return null;
+		checkValidity(conversionName, inFile, outFile, secondColumn);
+
+
+		ProfileDataConverter PDC = new ProfileDataConverter();
+
+				switch (conversionName) {
+				case "wigToSgr":
+					PDC.wigToSgr(inFile, outFile);
+
+					break;
+				case "wigToBed":
+					PDC.wigToBed(inFile, outFile, secondColumn);
+					break;
+				case "bedToSgr":
+						PDC.bedToSgr(inFile, outFile);
+					break;
+				case "sgrToBed":
+					PDC.sgrToBed(inFile, outFile, secondColumn);
+					break;
+				case "gff3ToBed":
+					PDC.gff3ToBed(inFile, outFile, secondColumn);
+					break;
+				case "gff3ToSgr":
+						PDC.gff3ToSgr(inFile, outFile);
+					break;
+				default:
+					throw new IllegalArgumentException();
+				}
+		return "success";
+
 	}
 
-	public String executeRegionDataConversion(String conversionName, String inFile){
-		switch(conversionName){
+	private boolean checkValidity(String conversionName, String inFile,
+			String outFile, String secondColumn) {
+
+		if (conversionName == null || inFile == null || outFile == null) {
+			throw new IllegalArgumentException();
+		}
+		if ((conversionName == "wigToBed" || conversionName == "sgrToBed" || conversionName == "gff3ToBed")
+				&& secondColumn == null) {
+			throw new IllegalArgumentException();
+		}
+		File f = new File(outFile);
+		if(f.isDirectory()) {
+			throw new IllegalArgumentException();
+		}
+		return true;
+	}
+
+
+
+	public String executeRegionDataConversion(String conversionName,
+			String inFile) {
+		switch (conversionName) {
 		case "":
 			break;
 
-		default: throw new IllegalArgumentException();
+		default:
+			throw new IllegalArgumentException();
 		}
 		return null;
 	}
 
-	public void executeGenomeReleaseConversion(String chainfilePath, String infilePath,String outfilePath,String unliftedPath){
-		String fileType = checkFileType(infilePath);
-		String path = infilePath;
-		GenomeReleaseConverter genomeConverter = new GenomeReleaseConverter();
-		ProfileDataConverter typeConverter = new ProfileDataConverter();
-
-		// turn the inputfile to bed
-		switch(fileType){
-		case "wig":
-			path = typeConverter.wigToBed(infilePath);
-			break;
-		case "sgr":
-			path = typeConverter.sgrToBed(infilePath);
-			break;
-		case "gff3":
-			path = typeConverter.gff3ToBed(infilePath);
-		case "bed":
-			break;
-		default:
-			throw new IllegalArgumentException();
-		}
-
-
-		String outfileBed = outfilePath.split("\\.")[0];
-		outfileBed = outfileBed+".bed";
-
-		genomeConverter.procedure(path, outfileBed, chainfilePath, unliftedPath);
-
-		//convert the outputfile to the inputfiles original filetype
-		switch(fileType){
-		case "wig":
-			typeConverter.bedToWig(outfileBed);
-			break;
-		case "sgr":
-			typeConverter.bedToSgr(outfileBed);
-			break;
-		case "gff3":
-			typeConverter.bedToGff3(outfileBed);
-		case "bed":
-			break;
-		default:
-			throw new IllegalArgumentException();
-		}
-
-		//delete the bedfile
-		File file = new File(outfileBed);
-		file.delete();
-
-		}
-	public String checkFileType(String filePath){
-		String type = "";
-		if((filePath.length()-filePath.replaceAll("\\.","").length())>=1){
-			String[] splitArray = filePath.split("\\.");
-			int lengthOfArray = splitArray.length;
-			type = splitArray[lengthOfArray-1];
-			System.err.println(type);
-		}
-
-
-
-		return type;
-
-
-
-	}
+	// public void executeGenomeReleaseConversion(String chainfilePath, String
+	// infilePath,String outfilePath){
+	// String fileType = checkFileType(infilePath);
+	// String path = infilePath;
+	// GenomeReleaseConverter genomeConverter = new GenomeReleaseConverter();
+	// ProfileDataConverter typeConverter = new ProfileDataConverter();
+	//
+	// // turn the inputfile to bed
+	// switch(fileType){
+	// case "wig":
+	// path = typeConverter.wigToBed(infilePath);
+	// break;
+	// case "sgr":
+	// path = typeConverter.sgrToBed(infilePath);
+	// break;
+	// case "gff3":
+	// path = typeConverter.gff3ToBed(infilePath);
+	// case "bed":
+	// break;
+	// default:
+	// throw new IllegalArgumentException();
+	// }
+	//
+	//
+	// String outfileBed = outfilePath.split("\\.")[0];
+	// outfileBed = outfileBed+".bed";
+	//
+	// genomeConverter.procedure(path, outfileBed, chainfilePath);
+	//
+	// //convert the outputfile to the inputfiles original filetype
+	// switch(fileType){
+	// case "wig":
+	// typeConverter.bedToWig(outfileBed);
+	// break;
+	// case "sgr":// public void executeGenomeReleaseConversion(String
+	// chainfilePath, String infilePath,String outfilePath){
+	// String fileType = checkFileType(infilePath);
+	// String path = infilePath;
+	// GenomeReleaseConverter genomeConverter = new GenomeReleaseConverter();
+	// ProfileDataConverter typeConverter = new ProfileDataConverter();
+	//
+	// // turn the inputfile to bed
+	// switch(fileType){
+	// case "wig":
+	// path = typeConverter.wigToBed(infilePath);
+	// break;
+	// case "sgr":
+	// path = typeConverter.sgrToBed(infilePath);
+	// break;
+	// case "gff3":
+	// path = typeConverter.gff3ToBed(infilePath);
+	// case "bed":
+	// break;
+	// default:
+	// throw new IllegalArgumentException();
+	// }
+	//
+	//
+	// String outfileBed = outfilePath.split("\\.")[0];
+	// outfileBed = outfileBed+".bed";
+	//
+	// genomeConverter.procedure(path, outfileBed, chainfilePath);
+	//
+	// //convert the outputfile to the inputfiles original filetype
+	// switch(fileType){
+	// case "wig":
+	// typeConverter.bedToWig(outfileBed);
+	// break;
+	// case "sgr":
+	// typeConverter.bedToSgr(outfileBed);
+	// break;
+	// case "gff3":
+	// typeConverter.bedToGff3(outfileBed);
+	// case "bed":
+	// break;
+	// default:
+	// throw new IllegalArgumentException();
+	// }
+	//
+	// //delete the bedfile
+	// File file = new File(outfileBed);
+	// file.delete();
+	//
+	// }
+	// public String checkFileType(String filePath){
+	// String type = "";
+	// if((filePath.length()-filePath.replaceAll("\\.","").length())==1){
+	// type = filePath.split("\\.", 10)[1];
+	// System.err.println(type);
+	// }
+	//
+	//
+	//
+	// return type;
+	//
+	//
+	//
+	// }
+	// typeConverter.bedToSgr(outfileBed);
+	// break;
+	// case "gff3":
+	// typeConverter.bedToGff3(outfileBed);
+	// case "bed":
+	// break;
+	// default:
+	// throw new IllegalArgumentException();
+	// }
+	//
+	// //delete the bedfile
+	// File file = new File(outfileBed);
+	// file.delete();
+	//
+	// }
+	// public String checkFileType(String filePath){
+	// String type = "";
+	// if((filePath.length()-filePath.replaceAll("\\.","").length())==1){
+	// type = filePath.split("\\.", 10)[1];
+	// System.err.println(type);
+	// }
+	//
+	//
+	//
+	// return type;
+	//
+	//
+	//
+	// }
 }
