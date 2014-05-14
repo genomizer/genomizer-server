c406e3a4fc9aea4314b95e9fba8411e89ca1a43d
dv12can <calle.alexandersson93@gmail.com>
2014-05-14 16:48:03 +0200

    worked on test

--- src/testSuite/unitTests/PubMedIsValidTest.java
+++ src/testSuite/unitTests/PubMedIsValidTest.java
@@ -2,6 +2,7 @@ package testSuite.unitTests;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
 import org.junit.AfterClass;
@@ -29,13 +30,42 @@ public class PubMedIsValidTest {
     	ti.removeTuples();
     }
 
-    @Test
-    public void isValid(){
-    	assertTrue(dbac.isPubMedStringValid("Exp1[ExpID]"));
+    public boolean isPubMedStringValid(String pubMedString) throws IOException{
+
+    	int squareBracketsStart=0, squareBracketsStop=0;
+
+    	for(int i=0;i<pubMedString.length();i++){
+        	System.out.print(pubMedString.charAt(i));
+
+        	if (pubMedString.charAt(i) == '['){
+        		squareBracketsStart++;
+
+        	} else if (pubMedString.charAt(i) == ']') {
+        		squareBracketsStop++;
+        	}
+    	}
+
+    	System.out.println(" ");
+
+    	if(squareBracketsStart==squareBracketsStop){
+    		return true;
+    	}else{
+    		throw new IOException("Ugly PubMed String");
+    	}
+
     }
 
     @Test
-    public void isNotValid(){
-    	assertFalse(dbac.isPubMedStringValid("Exp1[ExpID"));
+    public void isValid() throws IOException{
+
+		assertTrue(isPubMedStringValid("Exp1[ExpID]"));
+
+    }
+
+    @Test(expected=IOException.class)
+    public void isNotValid() throws IOException{
+
+		isPubMedStringValid("Exp1[ExpID");
+
     }
 }
