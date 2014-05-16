a45b4aab4d2867f70d73575575c8ec2b4a60fb1b
tfy09jnn <tfy09jnn@outlook.com>
2014-05-16 09:22:04 +0200

    Fixed response.

---
+++ src/server/test/ServerGenomeReleaseTest.java
@@ -0,0 +1,76 @@
+package server.test;
+
+import static org.junit.Assert.*;
+
+import java.net.HttpURLConnection;
+
+import org.junit.Test;
+
+import com.google.gson.JsonObject;
+
+/**
+ * Class used to test Genome release add and delete.
+ *
+ * @author tfy09jnn
+ * @version 1.0
+ */
+public class ServerGenomeReleaseTest extends ServerAbstractTestClass {
+
+
+	/**
+	 * Method used to test that a genome release can be added
+	 * and returns the proper response code.
+	 * @throws Exception
+	 */
+	@Test
+	public void testAddGenomeRelease() throws Exception {
+
+		//Create JSON login object.
+		JsonObject jj = new JsonObject();
+		jj.addProperty("username", "jonas");
+		jj.addProperty("password", "losenord");
+		sendLogin(jj);
+
+		//Get connection and then add headers.
+		HttpURLConnection con = connect("POST", serverURL + "/genomeRelease");
+		con.setRequestProperty("Content-Type", "application/json");
+		con.setRequestProperty("Authorization", token.getToken());
+
+		String json_output = "{\"fileName\":\"com_TestFile\", \"specie\":\"human\",\"genomeVersion\": \"hg38\"}";
+		
+		sendResponseString(con, json_output);
+		
+		String response = getResponseString(con);
+		
+		System.out.println("Response filename is: " + response);
+		
+		
+	}
+
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
