0d178cc9baedc712c0ea0aff7e7d0a3739348c7c
c11tsg <c11tsg@cs.umu.se>
2014-05-16 13:10:41 +0200

    Refactor. Commented. Slick N slim. Throws more stuff upwards aswell.

--- src/process/classes/SmoothingAndStep.java
+++ src/process/classes/SmoothingAndStep.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -21,70 +22,69 @@ public class SmoothingAndStep {
     private int stepSize;
 
 
-    public double smoothing(int[] params, String inPath, String outPath, int stepSize) {
+    /*
+     * Parameters:
+     * Params:	  An array with 5 integers representing parameters.
+     * params[0]: Window Size, the number of signal values that the smoothing
+     * 		  should be calculated on.
+     * params[1]: Wether the smoothing should be trimmed mean (0) or median (1)
+     * params[2]: Minimum numbers to smooth. A number that says how many signal
+     * 		  values the program at least need in order to smooth one row.
+     * 		  This number must be smaller than windowSize
+     * params[3]: Can either be 1 or 0. If 1 the program will calculate the
+     * 		  total mean value for all rows and print those.
+     * params[4]: Print zeroes. If the program should print rows where the
+     * 		  signal value is 0 the flag should be (1), if (0) the program
+     * 		  will not print the zeroes.
+     *
+     * inPath:	  A string that tells the location and name of the source file.
+     * outPath:	  A string that tells the location and name of the output file.
+     *
+     * stepSize:  Flag that tells if the user wants to do stepping. Should be
+     * 		  1 if the user wants no stepping. Have to be larger than 0.
+     */
+    public double smoothing(int[] params, String inPath, String outPath, int stepSize) throws IllegalAccessException, IOException {
+	if (params==null||params[2]>=params[0] || stepSize < 1 || params[0] < 1 ){
+	    throw new IllegalArgumentException();
+	}
+
 	data = new ArrayList<Tuple>();
 	readSumValue = 0;
 	noOfValues = 0;
+	String strLine;
 	this.stepSize = stepSize;
 
-	if (params==null||params[2]>=params[0]){
-	    throw new IllegalArgumentException();
+	setupBuffertReader(inPath);
+	setupBuffertWriter(outPath);
+
+	for(int i = 0; i<params[0]; i++){
+	    if((strLine = br.readLine()) != null){
+		addLine(strLine);
+	    }
 	}
-	try{
-	    FileInputStream fstream = new FileInputStream(inPath);
-	    //    FileInputStream fstream = new FileInputStream("test.sgr");
-	    DataInputStream in = new DataInputStream(fstream);
-	    br = new BufferedReader(new InputStreamReader(in));
+	smoothOneRow(params,params[0]);
 
-	    String strLine;
-	    bw = setupBuffertWriter(outPath);
-	    // bw = setupBuffertWriter("outfile.sgr");
-	    for(int i = 0; i<params[0]; i++){
-		if((strLine = br.readLine()) != null){
-		    addLine(strLine);
-		}
-	    }
-
+	while((strLine = br.readLine()) != null){
+	    shiftLeft(strLine, params);
 	    smoothOneRow(params,params[0]);
+	}
 
-	    while((strLine = br.readLine()) != null){
-		shiftLeft(strLine, params);
-		smoothOneRow(params,params[0]);
-	    }
+	data.remove(0);
+	int dataSize = data.size();
+	for (int i = 0;i<(dataSize-params[2]);i++){
+	    smoothOneRow(params,dataSize-i);
+	    data.remove(0);
+	}
+	dataSize = data.size();
+	for (int j = 0; j < dataSize;j++){
+	    writeToFile(params);
 
 	    data.remove(0);
-	    int dataSize = data.size();
-	    for (int i = 0;i<(dataSize-params[2]);i++){
-		smoothOneRow(params,dataSize-i);
-		data.remove(0);
-	    }
-	    dataSize = data.size();
-	    for (int j = 0; j < dataSize;j++){
-		writeToFile(params);
-
-		data.remove(0);
-	    }
-	    bw.close();
-	    in.close();
-	}catch (Exception e){//Catch exception if any
-	    e.printStackTrace();
 	}
-	System.out.println(readSumValue/noOfValues);
+	bw.close();
+	br.close();
 	data.clear();
 	return readSumValue/noOfValues;
-    }
-
-    private void writeToFile(int[] params) throws IOException {
-	if(!((params[4] == 0) && (data.get(0).getNewSignal() == 0)) ){
-	    if(data.get(0).getPosition()%stepSize== 0){
-		bw.write(data.get(0).toString());
-	    }
-	}
-	if (params[3] == 1){
-	    System.out.println(data.get(0).getPosition());
-	    readSumValue = readSumValue+ data.get(0).getSignal();
-	    noOfValues++;
-	}
     }
 
     private void smoothOneRow(int[] params, int noOfRowsToSmooth) throws IllegalAccessException, IOException {
@@ -95,9 +95,54 @@ public class SmoothingAndStep {
 	} else {
 	    throw new IllegalAccessException();
 	}
-
 	writeToFile(params);
+    }
 
+    private void smoothTrimmedMean(int noOfRowsToSmooth, int[] params) {
+	int minNrSigs = 1;
+	double meanSignal = data.get(0).getSignal();
+	double maxSig = meanSignal;
+	double minSig = meanSignal;
+
+	if(data.size() >= params[2]){
+	    for(int j = 1 ; j < noOfRowsToSmooth; j++){
+		meanSignal = meanSignal + data.get(j).getSignal();
+		minNrSigs++;
+		if(maxSig < data.get(j).getSignal()){
+		    maxSig = data.get(j).getSignal();
+		}
+		if(minSig > data.get(j).getSignal()){
+		    minSig = data.get(j).getSignal();
+		}
+	    }
+	    calcAndSetMeanSignal(params, minNrSigs, meanSignal, maxSig,
+		    minSig);
+	}
+    }
+
+    private void smoothMedian(int noOfRowsToSmooth, int[] params) {
+	double[] array = new double [noOfRowsToSmooth];
+
+	if(data.size() >= params[2]){
+	    for(int j = 0 ; j < (noOfRowsToSmooth); j++){
+		if(data.size() > j){
+		    array[j] = data.get(j).getSignal();
+		}
+	    }
+	    data.get(0).setNewSignal(median(array));
+	}
+    }
+
+    private void writeToFile(int[] params) throws IOException {
+	if(!((params[4] == 0) && (data.get(0).getNewSignal() == 0)) ){
+	    if(data.get(0).getPosition()%stepSize== 0){
+		bw.write(data.get(0).toString());
+	    }
+	}
+	if (params[3] == 1){
+	    readSumValue = readSumValue+ data.get(0).getSignal();
+	    noOfValues++;
+	}
     }
 
     private void shiftLeft(String strLine, int[] params) throws IllegalAccessException, IOException {
@@ -127,51 +172,20 @@ public class SmoothingAndStep {
 	}
     }
 
-    private void smoothMedian(int noOfRowsToSmooth, int[] params) {
-	double[] array = new double [noOfRowsToSmooth];
-
-	if(data.size() >= params[2]){
-	    for(int j = 0 ; j < (noOfRowsToSmooth); j++){
-		if(data.size() > j){
-		    array[j] = data.get(j).getSignal();
-		}
-	    }
-	    data.get(0).setNewSignal(median(array));
-	}
-    }
-
     private void addLine(String strLine) {
 	data.add(new Tuple(strLine));
     }
 
-    private BufferedWriter setupBuffertWriter(String outPath) throws IOException {
+    private void setupBuffertWriter(String outPath) throws IOException {
 	File outFile = new File(outPath);
 	if (!outFile.exists()) {
 	    outFile.createNewFile();
 	}
-	return new BufferedWriter(new FileWriter(outFile.getAbsoluteFile()));
+	bw = new BufferedWriter(new FileWriter(outFile.getAbsoluteFile()));
     }
 
-    private void smoothTrimmedMean(int noOfRowsToSmooth, int[] params) {
-	int minNrSigs = 1;
-	double meanSignal = data.get(0).getSignal();
-	double maxSig = meanSignal;
-	double minSig = meanSignal;
-
-	if(data.size() >= params[2]){
-	    for(int j = 1 ; j < noOfRowsToSmooth; j++){
-		meanSignal = meanSignal + data.get(j).getSignal();
-		minNrSigs++;
-		if(maxSig < data.get(j).getSignal()){
-		    maxSig = data.get(j).getSignal();
-		}
-		if(minSig > data.get(j).getSignal()){
-		    minSig = data.get(j).getSignal();
-		}
-	    }
-	    calcAndSetMeanSignal(params, minNrSigs, meanSignal, maxSig,
-		    minSig);
-	}
+    private void setupBuffertReader(String inPath) throws FileNotFoundException{
+	br = new BufferedReader(new InputStreamReader(new DataInputStream(new FileInputStream(inPath))));
     }
 
     private void calcAndSetMeanSignal(int[] params,
