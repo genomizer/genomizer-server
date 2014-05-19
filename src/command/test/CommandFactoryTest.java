ae9c189c57535c436bed2cb2014f063a98742a59
tfy09jnn <tfy09jnn@outlook.com>
2014-05-19 10:54:58 +0200

    Added, fixed alot in commandfactoryTest.

--- src/command/test/CommandFactoryTest.java
+++ src/command/test/CommandFactoryTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import command.CommandFactory;
 
+//TODO: Implement missing unit tests, make sure all methods in commandfactory are tested.
 /**
  * Testclass used to test the CommandFactory class.
  *
@@ -93,44 +94,75 @@ public class CommandFactoryTest {
 	}
 
 	/**
-	 * Test creation of Retrieve experiment command and that it's not null.
+	 * Test creation of DeleteExperimentCommand and that it's not null.
 	 */
 	@Test
-	public void testCreateRetrieveExperimentCommand() {
-
-		CommandFactory cmdf = new CommandFactory();
-		String restful = "ABCDEF";
-		assertNotNull(cmdf.createGetExperimentCommand(restful));
-
+	public void testDeleteExperimentCommandNotNull() {
+		//TODO: Implement.
+		fail("Not yet implemented.");
 	}
 
 	/**
-	 * Test that creation of AddGenomeReleaseCommand is not
-	 * null.
+	 * Test creation of GetFileFromExperimentCommand and that it's not null.
 	 */
 	@Test
-	public void testCreateAddGenomeReleaseCommand() {
-
-		CommandFactory cmdf = new CommandFactory();
-		String json = "{\"fileName\":\"abc123\",\"specie\":\"human\",\"genomeVersion\":\"GV 1.0\"}";
-
-		assertNotNull(cmdf.createAddGenomeReleaseCommand(json));
-
+	public void testGetFileFromExperimentCommandNotNull() {
+		//TODO: Implement.
+		fail("Not yet implemented.");
 	}
 
 	/**
-	 * Test that creation of AddGenomeReleaseCommand is not
-	 * null.
+	 * Test creation of AddFileToExperimentCommand and that it's not null.
 	 */
 	@Test
-	public void testCreateDeleteGenomeReleaseCommand() {
+	public void testAddFileToExperimentCommandNotNull() {
+		//TODO: Implement.
+		fail("Not yet implemented.");
+	}
 
-		CommandFactory cmdf = new CommandFactory();
-		String specie = "tempSpecie";
-		String genomeVersion = "tmpGenomeVersion";
+	/**
+	 * Test creation of UpdateFileInExperimentCommand and that it's not null.
+	 */
+	@Test
+	public void testUpdateFileInExperimentCommandNotNull() {
+		//TODO: Implement.
+		fail("Not yet implemented.");
+	}
 
-		assertNotNull(cmdf.createDeleteGenomeReleaseCommand(specie, genomeVersion));
+	/**
+	 * Test creation of DeleteFileFromExperimentCommand and that it's not null.
+	 */
+	@Test
+	public void testDeleteFileFromExperimentCommandNotNull() {
+		//TODO: Implement.
+		fail("Not yet implemented.");
+	}
 
+	/**
+	 * Test creation of SearchForExperimentCommand and that it's not null.
+	 */
+	@Test
+	public void testSearchForExperimentCommandNotNull() {
+		//TODO: Implement.
+		fail("Not yet implemented.");
+	}
+
+	/**
+	 * Test creation of UpdateUserCommand and that it's not null.
+	 */
+	@Test
+	public void testUpdateUserCommandNotNull() {
+		//TODO: Implement.
+		fail("Not yet implemented.");
+	}
+
+	/**
+	 * Test creation of DeleteUserCommand and that it's not null.
+	 */
+	@Test
+	public void testDeleteUserCommandNotNull() {
+		//TODO: Implement.
+		fail("Not yet implemented.");
 	}
 
 	/**
@@ -149,6 +181,155 @@ public class CommandFactoryTest {
 		String username = "tmpUsername";
 		String parsedRestful = "parsed";
 		assertNotNull(cmdf.createProcessCommand(json, username, parsedRestful));
+
+	}
+
+	/**
+	 * Test creation of GetAnnotationInformationCommand and that it's not null.
+	 */
+	@Test
+	public void testGetAnnotationInformationCommandNotNull() {
+		//TODO: Implement.
+		fail("Not yet implemented.");
+	}
+
+	/**
+	 * Test creation of AddAnnotationFieldCommand and that it's not null.
+	 */
+	@Test
+	public void testAddAnnotationFieldCommandNotNull() {
+		//TODO: Implement.
+		fail("Not yet implemented.");
+	}
+
+	/**
+	 * Test creation of AddAnnotationValueCommand and that it's not null.
+	 */
+	@Test
+	public void testAddAnnotationValueCommandNotNull() {
+		//TODO: Implement.
+		fail("Not yet implemented.");
+	}
+
+	/**
+	 * Test creation of RemoveAnnotationFieldCommand and that it's not null.
+	 */
+	@Test
+	public void testRemoveAnnotationFieldCommandNotNull() {
+		//TODO: Implement.
+		fail("Not yet implemented.");
+	}
+
+	/**
+	 * Test creation of GetAnnotationPrivilegesCommand and that it's not null.
+	 */
+	@Test
+	public void testGetAnnotationPrivilegesCommandNotNull() {
+		//TODO: Implement.
+		fail("Not yet implemented.");
+	}
+
+	/**
+	 * Test creation of UpdateAnnotationPrivilegesCommand and that it's not null.
+	 */
+	@Test
+	public void testUpdateAnnotationPrivilegesCommandNotNull() {
+		//TODO: Implement.
+		fail("Not yet implemented.");
+	}
+
+	/**
+	 * Test that creation of AddGenomeReleaseCommand is not
+	 * null.
+	 */
+	@Test
+	public void testCreateAddGenomeReleaseCommand() {
+
+		CommandFactory cmdf = new CommandFactory();
+		String json = "{\"fileName\":\"abc123\",\"specie\":\"human\",\"genomeVersion\":\"GV 1.0\"}";
+
+		assertNotNull(cmdf.createAddGenomeReleaseCommand(json));
+
+	}
+
+	/**
+	 * Test creation of RenameAnnotationValueCommand and that it's not null.
+	 */
+	@Test
+	public void testRenameAnnotationValueCommandNotNull() {
+		//TODO: Implement.
+		fail("Not yet implemented.");
+	}
+
+	/**
+	 * Test that creation of AddGenomeReleaseCommand is not
+	 * null.
+	 */
+	@Test
+	public void testCreateDeleteGenomeReleaseCommand() {
+
+		CommandFactory cmdf = new CommandFactory();
+		String specie = "tempSpecie";
+		String genomeVersion = "tmpGenomeVersion";
+
+		assertNotNull(cmdf.createDeleteGenomeReleaseCommand(specie, genomeVersion));
+
+	}
+
+	/**
+	 * Test creation of DeleteAnnotationValueCommand and that it's not null.
+	 */
+	@Test
+	public void testDeleteAnnotationValueCommandNotNull() {
+		//TODO: Implement.
+		fail("Not yet implemented.");
+	}
+
+	/**
+	 * Test creation of EditAnnotationFieldCommand and that it's not null.
+	 */
+	@Test
+	public void testEditAnnotationFieldCommandNotNull() {
+		//TODO: Implement.
+		fail("Not yet implemented.");
+	}
+
+	/**
+	 * Test creation of GetAllGenomeReleasesCommand and that it's not null.
+	 */
+	@Test
+	public void testGetAllGenomeReleasesCommandNotNull() {
+		//TODO: Implement.
+		fail("Not yet implemented.");
+	}
+
+	/**
+	 * Test creation of GetGenomeReleaseSpeciesCommand and that it's not null.
+	 */
+	@Test
+	public void testGetGenomeReleaseSpeciesCommandNotNull() {
+		//TODO: Implement.
+		fail("Not yet implemented.");
+	}
+
+	/**
+	 * Test creation of GetProcessStatusCommand and that it's not null.
+	 */
+	@Test
+	public void testGetProcessStatusCommandNotNull() {
+		//TODO: Implement.
+		fail("Not yet implemented.");
+	}
+
+	/**
+	 * Test creation of Retrieve experiment command and that it's not null.
+	 */
+	@Test
+	public void testCreateRetrieveExperimentCommand() {
+
+		CommandFactory cmdf = new CommandFactory();
+		String restful = "ABCDEF";
+		assertNotNull(cmdf.createGetExperimentCommand(restful));
 
 	}
 
