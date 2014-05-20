f9128bdc546a771b9a9ed2b87216705516b71c87
tfy09jnn <tfy09jnn@outlook.com>
2014-05-20 09:19:56 +0200

    Added/implemented unit test for GetExperimentCommand.

--- src/command/test/GetExperimentCommandTest.java
+++ src/command/test/GetExperimentCommandTest.java
@@ -1,22 +1,38 @@
 package command.test;
 
 import static org.junit.Assert.*;
-
-
 import org.junit.Test;
-
-import response.Response;
-
-import command.CommandHandler;
 import command.GetExperimentCommand;
 
+/**
+ * Test class used to check that GetExperimentCommand works
+ * properly.
+ *
+ * @author tfy09jnn
+ * @version 1.0
+ */
 public class GetExperimentCommandTest {
 
+	/**
+	 * Test that checks that creation is not null.
+	 */
 	@Test
-	public void testAnnotations() {
-		CommandHandler handler = new CommandHandler();
-		GetExperimentCommand cmd = new GetExperimentCommand(handler.parseRest("/experiment/abc"));
-		Response rsp = cmd.execute();
+	public void testCreationNotNull() {
+
+		GetExperimentCommand cmd = new GetExperimentCommand("ExpId");
+		assertNotNull(cmd);
+
+	}
+
+	/**
+	 * Test that checks that validation is always true.
+	 */
+	@Test
+	public void testValidationAlwaysTrue() {
+
+		GetExperimentCommand cmd = new GetExperimentCommand("ExpId");
+		assertTrue(cmd.validate());
+
 	}
 
 }
