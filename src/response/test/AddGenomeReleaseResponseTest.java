45667f857f0255deebe3750a448777459a0ed486
tfy09jnn <tfy09jnn@outlook.com>
2014-05-22 15:32:44 +0200

    Added AddGenomeRelease response test. Fixed sourcecode. Only need some more unit tests and database method to call.

--- src/response/test/AddGenomeReleaseResponseTest.java
+++ src/response/test/AddGenomeReleaseResponseTest.java
@@ -2,6 +2,8 @@ package response.test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,7 +11,6 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import response.AddGenomeReleaseResponse;
-import response.Response;
 import response.StatusCode;
 
 /**
@@ -20,7 +21,6 @@ import response.StatusCode;
  */
 public class AddGenomeReleaseResponseTest {
 
-	//Builder used with almost all tests.
 	public Gson gson = null;
 
 	/**
@@ -35,41 +35,15 @@ public class AddGenomeReleaseResponseTest {
 
 	}
 
-	/**
-	 * Test used to check that the created response is not null.
-	 */
 	@Test
-	public void testCreateNotNull() {
+	public void testGetBody() {
 
-		Response rsp = new AddGenomeReleaseResponse(StatusCode.OK, "testpath");
-		assertNotNull(rsp);
-
-	}
-
-	/**
-	 * Test used to check that the created response with code
-	 * is retrievable.
-	 */
-	@Test
-	public void testGetResponseCode() {
-
-		Response rsp = new AddGenomeReleaseResponse(StatusCode.OK, "testpath");
-		assertEquals(StatusCode.OK, rsp.getCode());
-
-	}
-
-	/**
-	 * Test used to check that JSON de-serialization works properly.
-	 */
-	@Test
-	public void testResponseJSON() {
-
-		String json = "{\"filePath\":\"abc123\"}";
-		Response rsp = new AddGenomeReleaseResponse(StatusCode.OK, "ABC");
-		rsp = gson.fromJson(json, AddGenomeReleaseResponse.class);
-	    String json2 = gson.toJson(rsp);
-
-	    assertEquals(json2, json);
+		ArrayList<String> array = new ArrayList<String>();
+		array.add("FIRST");
+		array.add("SECOND");
+		AddGenomeReleaseResponse rsp = new AddGenomeReleaseResponse(StatusCode.CREATED, array);
+		String checker = rsp.getBody();
+		assertEquals(checker, "[\"FIRST\",\"SECOND\"]");
 
 	}
 
