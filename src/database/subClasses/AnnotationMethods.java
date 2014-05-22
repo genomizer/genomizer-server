532d284bff1f320f4a8adb30d2a4a92cf5fc11cf
Jonas Engbo <yhi04jeo@cs.umu.se>
2014-05-22 13:32:40 +0200

    changes for empty arguments

--- src/database/subClasses/AnnotationMethods.java
+++ src/database/subClasses/AnnotationMethods.java
@@ -24,7 +24,7 @@ import database.PubMedToSQLConverter;
  * Class that contains all the methods for adding,changing, getting and removing
  * Annotations in the database. This class is a subClass of
  * databaseAcessor.java.
- * 
+ *
  * date: 2014-05-14 version: 1.0
  */
 public class AnnotationMethods {
@@ -38,7 +38,7 @@ public class AnnotationMethods {
 
     /**
      * Constructor for the AnnotationMethod object.
-     * 
+     *
      * @param connection
      *            Connection, the connection to the database.
      */
@@ -53,7 +53,7 @@ public class AnnotationMethods {
 
     /**
      * Gets all the annotation possibilities from the database.
-     * 
+     *
      * @return annotations Map<String, Integer> a Map with the label string as
      *         key and datatype as value. The possible datatypes are FREETEXT
      *         and DROPDOWN.
@@ -83,7 +83,7 @@ public class AnnotationMethods {
 
     /**
      * Creates an Annotation object from an annotation label.
-     * 
+     *
      * @param label
      *            Stringthe name of the annotation to create the object for.
      * @return Annotation - the Annotation object. If the label does not exist,
@@ -115,7 +115,7 @@ public class AnnotationMethods {
 
     /**
      * Creates a list of Annotation objects from a list of annotation labels.
-     * 
+     *
      * @param labels
      *            the list of labels.
      * @return annotations List<Annotation> - will return a list with all the
@@ -147,7 +147,7 @@ public class AnnotationMethods {
     /**
      * Finds all annotationLabels that exist in the database, example of labels:
      * sex, tissue, etc...
-     * 
+     *
      * @return annotationLabels ArrayList<String>
      */
     public ArrayList<String> getAllAnnotationLabels() {
@@ -175,11 +175,11 @@ public class AnnotationMethods {
 
     /**
      * Gets the datatype of a given annotation.
-     * 
+     *
      * @param label
      *            annotation label.
      * @return integer - the annotation's datatype (FREETEXT or DROPDOWN).
-     * 
+     *
      * @throws SQLException
      *             if the query does not succeed
      */
@@ -196,7 +196,7 @@ public class AnnotationMethods {
     /**
      * Gets the default value for a annotation if there is one, If not it
      * returns NULL.
-     * 
+     *
      * @param annotationLabel
      *            String - the name of the annotation to check
      * @return DefaultValue String - The defult value or NULL.
@@ -225,7 +225,7 @@ public class AnnotationMethods {
     /**
      * Deletes an annotation from the list of possible annotations. Label
      * SPECIES can't be changed because of dependencies in other tables.
-     * 
+     *
      * @param label
      *            String - the label of the annotation to delete.
      * @return res integer - the number of tuples deleted in the database.
@@ -265,7 +265,7 @@ public class AnnotationMethods {
 
     /**
      * Adds a free text annotation to the list of possible annotations.
-     * 
+     *
      * @param label
      *            String the name of the annotation.
      * @param required
@@ -280,6 +280,9 @@ public class AnnotationMethods {
      */
     public int addFreeTextAnnotation(String label, String defaultValue,
             boolean required) throws SQLException, IOException {
+
+    	isValidArgument(label);
+    	isValidArgument(defaultValue);
 
         if (isFileAnnotation(label)) {
             throw new IOException(
@@ -319,7 +322,7 @@ public class AnnotationMethods {
 
     /**
      * Checks if a given annotation is required to be filled by the user.
-     * 
+     *
      * @param annotationLabel
      *            String - the name of the annotation to check
      * @return boolean - true if it is required, else false
@@ -348,7 +351,7 @@ public class AnnotationMethods {
     /**
      * Gets all the choices for a drop down annotation. Deprecated, use
      * {@link #getChoices(String) getChoices} instead.
-     * 
+     *
      * @param label
      *            String the drop down annotation to get the choice for.
      * @return theChoices ArrayList<String> - all the choices.
@@ -380,7 +383,7 @@ public class AnnotationMethods {
 
     /**
      * Adds a drop down annotation to the list of possible annotations.
-     * 
+     *
      * @param label
      *            String - the name of the annotation.
      * @param choices
@@ -395,6 +398,8 @@ public class AnnotationMethods {
     public int addDropDownAnnotation(String label, List<String> choices,
             int defaultValueIndex, boolean required) throws SQLException,
             IOException {
+
+    	isValidArgument(label);
 
         if (isFileAnnotation(label)) {
             throw new IOException(
@@ -465,7 +470,7 @@ public class AnnotationMethods {
 
     /**
      * Method to add a value to a existing DropDown annotation.
-     * 
+     *
      * @param label
      *            String , the label of the chosen DropDown annotation.
      * @param value
@@ -480,6 +485,9 @@ public class AnnotationMethods {
      */
     public int addDropDownAnnotationValue(String label, String value)
             throws SQLException, IOException {
+
+    	isValidArgument(label);
+    	isValidArgument(value);
 
         if (!isValidChoice(value)) {
             throw new IOException("Value contains invalid characters");
@@ -516,7 +524,7 @@ public class AnnotationMethods {
 
     /**
      * Method to remove a given annotation of a dropdown- annotation.
-     * 
+     *
      * @param label
      *            String - the label of the chosen annotation
      * @param value
@@ -580,16 +588,16 @@ public class AnnotationMethods {
 
     /**
      * Changes the annotation label.
-     * 
+     *
      * OBS! This changes the label for all experiments. Label SPECIES can't be
      * changed because of dependencies in other tables. If the Species label can
      * be changed to another, it becomes removable.
-     * 
+     *
      * @param oldLabel
      *            String
      * @param newLabel
      *            string
-     * 
+     *
      * @return res int - the number of tuples updated
      * @throws SQLException
      *             If the update fails
@@ -625,22 +633,22 @@ public class AnnotationMethods {
 
     /**
      * Changes the value of an annotation corresponding to it's label.
-     * 
+     *
      * Parameters: label of annotation, the old value and the new value to
      * change to.
-     * 
+     *
      * OBS! This method changes the value for every experiment.
-     * 
+     *
      * Throws an SQLException if the new value already exists in the choices
      * table (changing all males to female, and female is already in the table)
-     * 
+     *
      * @param label
      *            String - the label name.
      * @param oldValue
      *            String - the name of the old annotation value.
      * @param newValue
      *            String - the name of the new annotation value.
-     * 
+     *
      * @throws SQLException
      * @throws IOException
      * @throws ParseException
@@ -708,7 +716,7 @@ public class AnnotationMethods {
 
     /**
      * Gets all the choices for a drop down annotation.
-     * 
+     *
      * @param label
      *            String - the drop down annotation to get the choice for.
      * @return choices List<String> - the choices for one annotation label.
@@ -740,7 +748,7 @@ public class AnnotationMethods {
      * binds a sql prepared query statement with parameters, example:
      * "UPDATE Annotation_Choices SET Value = ? WHERE Label = ? and Value = ?;"
      * and the questionmarks are the parameters.
-     * 
+     *
      * @param query
      *            PreparedStatement
      * @param params
@@ -803,7 +811,7 @@ public class AnnotationMethods {
     /**
      * private method to check if the annotation contains invalid characters
      * ('(', ')', '[' and ']'.
-     * 
+     *
      * @param annotation
      * @return
      */
@@ -819,5 +827,11 @@ public class AnnotationMethods {
         }
 
         return true;
+    }
+
+    private void isValidArgument(String arg) throws IOException {
+    	if (arg.contentEquals("") || arg == null) {
+    		throw new IOException("Invalid argument(s)");
+    	}
     }
 }
