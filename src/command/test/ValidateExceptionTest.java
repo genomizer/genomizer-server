6ac69f7092213d52e3d2ca9df0e7747bcdcf8108
tfy09jnn <tfy09jnn@outlook.com>
2014-05-26 14:38:27 +0200

    Added JUnit testcase for the exception class.

--- src/command/test/ValidateExceptionTest.java
+++ src/command/test/ValidateExceptionTest.java
@@ -1,8 +1,8 @@
 package command.test;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+import command.ValidateException;
 
 /**
  * Class used to test that the validateException class
@@ -13,9 +13,52 @@ import org.junit.Test;
  */
 public class ValidateExceptionTest {
 
+	/**
+	 * Test used to check that the exception is created succesfully
+	 * if there's no parameters passed and that it's not null.
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCreateEmptyNotNull() {
+
+		ValidateException e = new ValidateException();
+		assertNotNull(e);
+
+	}
+
+	/**
+	 * Test used to check that the exception is created successfully
+	 * if the constructor with two parameters is used.
+	 */
+	@Test
+	public void testCreateParamConstructorNotNull() {
+
+		ValidateException e = new ValidateException(401, "hello");
+		assertNotNull(e);
+
+	}
+
+	/**
+	 * Test used to check that the code can be set properly, and that it's
+	 * retrievable with the getCode method.
+	 */
+	@Test
+	public void testGetCodeMethod() {
+
+		ValidateException e = new ValidateException(401, "hello");
+		assertTrue(e.getCode() == 401);
+
+	}
+
+	/**
+	 * Test used to check that the message can be set properly, and that it's
+	 * retrievable with the getMessage method.
+	 */
+	@Test
+	public void testGetMessageMethod() {
+
+		ValidateException e = new ValidateException(401, "hello");
+		assertEquals(e.getMessage(), "hello");
+
 	}
 
 }
