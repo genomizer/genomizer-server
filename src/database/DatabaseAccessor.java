d5ec69fe65e0e67c9f511b93d9a75745ffc7a790
Ruaridh Watt <ruaridh.watt@gmail.com>
2014-05-26 13:26:52 +0200

    Error messages.

--- src/database/DatabaseAccessor.java
+++ src/database/DatabaseAccessor.java
@@ -247,8 +247,16 @@ public class DatabaseAccessor {
      * @throws SQLException
      *             - if the query does not succeed
      */
-    public void deleteUser(String username) throws SQLException {
-        userMethods.deleteUser(username);
+    /**
+     * Deletes a user from the database.
+     *
+     * @param username
+     * @return The number of tuples deleted.
+     * @throws SQLException
+     *             If the database could not be reached.
+     */
+    public int deleteUser(String username) throws SQLException {
+        return userMethods.deleteUser(username);
     }
 
     /**
@@ -721,6 +729,7 @@ public class DatabaseAccessor {
 
     /**
      * Sets the status of a file to "Done".
+     *
      * NOT USED IN 2014!
      *
      * @param fileID
@@ -1011,6 +1020,8 @@ public class DatabaseAccessor {
 
     /**
      * Sets the status for a genome release file to "Done".
+     *
+     * NOT USED IN 2014!
      *
      * @param version
      *            the file version.
