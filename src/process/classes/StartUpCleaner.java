24dfffa1d47c8f32cc32cc65313500d1750228f0
Oskar Abrahamsson <c11oan@cs.umu.se>
2014-05-28 11:24:41 +0200

    created class that removes temporary files on start up

--- src/process/classes/StartUpCleaner.java
+++ src/process/classes/StartUpCleaner.java
@@ -1,14 +1,54 @@
 package process.classes;
 
-public class StartUpCleaner {
-	
-	private StartUpCleaner(){
-		
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
+public class StartUpCleaner{
+	private String resDir = "resources/";
+	private StartUpCleaner() {
+
 	}
-	
-	
-	public StartUpCleaner startUpCleanerFactory() {
+
+
+	public static StartUpCleaner startUpCleanerFactory() {
 		return new StartUpCleaner();
 	}
-	
+
+	public void removeOldTempDirectories(String dir) {
+		if(dir != null) {
+			System.out.println("dir var inte null");
+			File dirFile = new File(dir);
+			if(dirFile.isDirectory()) {
+				File[] listOfFiles = dirFile.listFiles();
+				for (File file : listOfFiles) {
+					System.out.println(file.toString());
+					if(file.getName().startsWith("results_") && file.isDirectory()) {
+								try {
+									delete(file);
+								} catch (IOException e) {
+									System.out.println("IO");
+								}
+
+					}
+
+				}
+			}
+		}
+
+	}
+
+
+	private void delete(File f) throws IOException {
+		  if (f.isDirectory()) {
+			    for (File c : f.listFiles()) {
+			    	delete(c);
+			    }
+			    f.delete();
+		  } else {
+			  f.delete();
+		  }
+
+	}
 }
