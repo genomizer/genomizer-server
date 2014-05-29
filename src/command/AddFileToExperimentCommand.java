593969e8060165e44dbe0e3f08a771365d38cbea
splutt <c11rog@cs.umu.se>
2014-05-29 16:41:10 +0200

    f

--- src/command/AddFileToExperimentCommand.java
+++ src/command/AddFileToExperimentCommand.java
@@ -94,7 +94,7 @@ public class AddFileToExperimentCommand extends Command {
 			throw new ValidateException(StatusCode.BAD_REQUEST, "Genome version: " + grVersion + " has to be between 1 and "
 					+ database.constants.MaxSize.FILE_GRVERSION + " characters long.");
 		}
-		if(uploader != null){
+		if(metaData != null){
 			if(metaData.length() > MaxSize.FILE_METADATA || metaData.length() < 1) {
 				throw new ValidateException(StatusCode.BAD_REQUEST, "Metadata has to be between 1 and "
 						+ database.constants.MaxSize.FILE_GRVERSION + " characters long.");
