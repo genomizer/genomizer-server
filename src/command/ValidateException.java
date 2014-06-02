f66a9dcb584511a7d9a30ebccd78095419413b57
tfy09jnn <tfy09jnn@outlook.com>
2014-06-02 09:59:57 +0200

    Commented ValidateException class.

--- src/command/ValidateException.java
+++ src/command/ValidateException.java
@@ -4,22 +4,19 @@ package command;
  * Class used to store exception messages that should be thrown
  * when a command class validate method fails.
  *
- * @author tfy09jnn
+ * @author Kommunikation/kontroll 2014.
  * @version 1.0
  */
 public class ValidateException extends Exception {
 
 	private int exceptionCode = 0;
-
-	/**
-	 *
-	 */
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * Empty constructor.
 	 */
 	public ValidateException() {
+
 	}
 
 	/**
@@ -29,16 +26,21 @@ public class ValidateException extends Exception {
 	 * @param message to insert.
 	 */
 	public ValidateException(int exceptionCode, String message) {
+
 		super(message);
 		this.exceptionCode = exceptionCode;
+
 	}
 
 	/**
-	 * method used to
-	 * @return
+	 * Method used to get the code that is set.
+	 *
+	 * @return the Code that is set.
 	 */
 	public int getCode() {
+
 		return exceptionCode;
+
 	}
 
 }
