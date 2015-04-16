cb5c439b6ca5373767b2f362eee1879c0300d5c1
dv13esn <dv13esn@cs.umu.se>
2015-04-16 11:22:40 +0200

    Test

--- test/database/test/TestInitializer.java
+++ test/database/test/TestInitializer.java
@@ -19,7 +19,7 @@ import org.junit.Ignore;
 
 import database.DatabaseAccessor;
 import database.test.unittests.SearchDatabaseTests;
-
+/** */
 /**
  * Create an instance of this class if you want to use the test tuples in
  * add_test_tuples.sql.
