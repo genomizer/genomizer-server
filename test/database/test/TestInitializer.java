6b9c81dc3e49dc226a0b76e833497938173616d1
Mikhail Glushenkov <c05mgv@cs.umu.se>
2015-04-17 10:17:48 +0200

    System.getEnv can return null.

--- test/database/test/TestInitializer.java
+++ test/database/test/TestInitializer.java
@@ -35,20 +35,21 @@ public class TestInitializer {
     public static String database;
 
     static {
-      if(System.getenv("TRAVIS").equals("true")) {
-        // Running on Travis.
-        username = "postgres";
-        password = "";
-        host     = "localhost";
-        database = "c5dv151_vt14";
-      }
-      else {
-        // Running in a CS lab.
-        username = "c5dv151_vt14";
-        password = "shielohh";
-        host     = "postgres";
-        database = "c5dv151_vt14";
-      }
+        String travis = System.getenv("TRAVIS");
+        if(travis != null && travis.equals("true")) {
+            // Running on Travis.
+            username = "postgres";
+            password = "";
+            host     = "localhost";
+            database = "c5dv151_vt14";
+        }
+        else {
+            // Running in a CS lab.
+            username = "c5dv151_vt14";
+            password = "shielohh";
+            host     = "postgres";
+            database = "c5dv151_vt14";
+        }
     }
 
 //    public static String username = "genomizer";
