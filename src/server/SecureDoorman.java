dc3b570433682b6bc7b25837022ed691ac092e48
ens11afk <ens11afk@cs.umu.se>
2015-04-20 15:49:41 +0200

    Added comments and cleaned up the code in the Response package

--- src/server/SecureDoorman.java
+++ src/server/SecureDoorman.java
@@ -32,6 +32,7 @@ import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.URLDecoder;
 import java.security.*;
+import java.security.cert.CertificateException;
 import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.Executor;
@@ -84,13 +85,14 @@ System.err.println("Https server started");
 
 		SSLContext sslContext = null;
 		char[] charPassword = password.toCharArray();
+		String filename = "genoStore";
 		try {
 			//Initialize context
 			sslContext = SSLContext.getInstance("TLSv1.1");
 
 			//Initialize the key store
 			KeyStore keyStore = KeyStore.getInstance("JKS");
-			String keyStoreName = "genoStore";
+			String keyStoreName = filename;
 			keyStore.load(new FileInputStream(keyStoreName),charPassword);
 
 			//Setup for the key manager factory
@@ -104,8 +106,34 @@ System.err.println("Https server started");
 			//Initialize the sslContext
 			sslContext.init(kmf.getKeyManagers(),tmf.getTrustManagers(),null);
 
-		} catch (Exception e) {
+		} catch (KeyStoreException e) {
 			e.printStackTrace();
+			System.err.println("The provider for the KeyStore is not available.");
+			System.exit(1);
+		} catch (CertificateException e) {
+			e.printStackTrace();
+			System.err.println("Could not load the key from the KeyStore.");
+			System.exit(1);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			System.err.println("Could not find the KeyStore algorithm.");
+			System.exit(1);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.err.println("Could not find the file: " + filename + ".");
+			System.exit(1);
+		} catch (UnrecoverableKeyException e) {
+			e.printStackTrace();
+			System.err.println("The key could not be retrieved.\n Please check if the password is correct.");
+			System.exit(1);
+		} catch (KeyManagementException e) {
+			e.printStackTrace();
+			System.err.println("Could not initialize the SSL context for the server.");
+			System.exit(1);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.err.println("Could not open the file: " + filename + ".");
+			System.exit(1);
 		}
 
 		//Creates the configurator object
@@ -127,8 +155,10 @@ System.err.println("Https server started");
 					sslParameters.setProtocols(sslEngine.getEnabledProtocols());
 					params.setSSLParameters(sslParameters);
 
-				} catch (Exception e) {
+				} catch (NoSuchAlgorithmException e) {
 					e.printStackTrace();
+					System.err.println("Could not find the KeyStore algorithm.");
+					System.exit(1);
 				}
 			}
 		};
