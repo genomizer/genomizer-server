4365c95dce00b308feff897e59978fd76b73fdad
Mikhail Glushenkov <c05mgv@cs.umu.se>
2015-04-23 14:36:46 +0200

    Unbreak the build.

--- src/response/GetProcessStatusResponse.java
+++ src/response/GetProcessStatusResponse.java
@@ -42,7 +42,7 @@ public class GetProcessStatusResponse extends Response {
 	@Override
 	public String getBody() {
 
-		if (procStats == null) {
+		if (processStatus == null) {
 			ErrorLogger.log("SYSTEM", "GetProcessStatusResponse.getBody(): " +
 					"Error getting process status");
 			return "";
