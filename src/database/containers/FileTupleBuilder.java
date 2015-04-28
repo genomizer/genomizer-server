73a26e2e95bc05d6a2163d8bdff14bf7d96e6ff3
Erik Samuelsson <dv13esn@cs.umu.se>
2015-04-28 15:26:50 +0200

    Rename methods

--- src/database/containers/FileTupleBuilder.java
+++ src/database/containers/FileTupleBuilder.java
@@ -3,92 +3,90 @@ package database.containers;
 import java.util.Date;
 
 /**
+ * Builder to create a FileTuple object.
+ *
  * Created by dv13esn on 2015-04-28.
  */
 public class FileTupleBuilder {
     
     private FileTuple product;
-
-    private Integer id;
-    private String path;
-    private String inputFilePath;
-    private String type;
-    private String filename;
-    private Date date;
-    private String metaData;
-    private String author;
-    private String uploader;
-    private Boolean isPrivate;
-    private String expId;
-    private String grVersion;
-    private String status;
     
     public FileTupleBuilder fileTuple(){
         product = new FileTuple();
         return this;
-    } 
+    }
 
-    public FileTupleBuilder setId(Integer id) {
+    /**
+     *
+     * @param id The ID of the File contained by the FileTuple.
+     * @return
+     */
+    public FileTupleBuilder withId(Integer id) {
         product.setId(id);
         return this;
     }
 
-    public FileTupleBuilder setPath(String path) {
+    /**
+     *
+     * @param path The path to the File
+     * @return
+     */
+    public FileTupleBuilder withPath(String path) {
         product.setPath(path);
         return this;
     }
 
-    public FileTupleBuilder setInputFilePath(String inputFilePath) {
+    public FileTupleBuilder withInputFilePath(String inputFilePath) {
         product.setInputFilePath(inputFilePath);
         return this;
     }
 
-    public FileTupleBuilder setType(String type) {
+    public FileTupleBuilder withType(String type) {
         product.setType(type);
         return this;
     }
 
-    public FileTupleBuilder setFilename(String filename) {
+    public FileTupleBuilder withFilename(String filename) {
         product.setFilename(filename);
         return this;
     }
 
-    public FileTupleBuilder setDate(Date date) {
+    public FileTupleBuilder withDate(Date date) {
         product.setDate(date);
         return this;
     }
 
-    public FileTupleBuilder setMetaData(String metaData) {
+    public FileTupleBuilder withMetaData(String metaData) {
         product.setMetaData(metaData);
         return this;
     }
 
-    public FileTupleBuilder setAuthor(String author) {
+    public FileTupleBuilder withAuthor(String author) {
         product.setAuthor(author);
         return this;
     }
 
-    public FileTupleBuilder setUploader(String uploader) {
+    public FileTupleBuilder withUploader(String uploader) {
         product.setUploader(uploader);
         return this;
     }
 
-    public FileTupleBuilder setIsPrivate(Boolean isPrivate) {
+    public FileTupleBuilder withIsPrivate(Boolean isPrivate) {
         product.setIsPrivate(isPrivate);
         return this;
     }
 
-    public FileTupleBuilder setExpId(String expId) {
+    public FileTupleBuilder withExpId(String expId) {
         product.setExpId(expId);
         return this;
     }
 
-    public FileTupleBuilder setGrVersion(String grVersion) {
+    public FileTupleBuilder withGrVersion(String grVersion) {
         product.setGrVersion(grVersion);
         return this;
     }
 
-    public FileTupleBuilder setStatus(String status) {
+    public FileTupleBuilder withStatus(String status) {
         product.setStatus(status);
         return this;
     }
