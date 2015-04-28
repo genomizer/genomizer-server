57d40b7ee6b5d5c4182c1841bcaefb1e117006e1
Tim Hedberg <dv13thg@cs.umu.se>
2015-04-28 14:52:29 +0200

    changed user password

--- sql/add_test_tuples.sql
+++ sql/add_test_tuples.sql
@@ -64,7 +64,7 @@ INSERT INTO Annotated_With  VALUES('Exp3', 'Species', 'Rat');
 INSERT INTO Annotated_With  VALUES('Exp3', 'Development Stage', 'Child');
 
 -- This creates a user called "testuser" with the obvious password
-INSERT INTO User_Info VALUES('testuser', '$2a$10$DepSfvFFOsrP/M6uLKXd5.8OiEQET4.vC9LqBUUt1p0VQXak2B0GO', '$2a$10$DepSfvFFOsrP/M6uLKXd5.8OiEQET4.vC9LqBUUt1p0VQXak2B0GO', 'admin','Test User','test.user.pvt@cs.umu.se');
+INSERT INTO User_Info VALUES('testuser', '$2a$10$i31YyxPjtntIVvslbM.esOKv4TaWzssP.MkHw5DpOCs3NhJp4goKq', '$2a$10$i31YyxPjtntIVvslbM.esOKv4TaWzssP.MkHw5DpOCs3NhJp4goKq', 'admin','Test User','test.user.pvt@cs.umu.se');
 
 INSERT INTO File VALUES(DEFAULT, '/var/www/data/Exp1/raw/file1.fastq', 'Raw', 'file1.fastq', CURRENT_TIMESTAMP, NULL, '/var/www/data/Exp1/raw/file1_input.fastq', 'Umeå Uni', 'user1', 'True', 'Exp1', NULL, DEFAULT);
 
