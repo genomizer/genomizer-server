56972ceb3c4bcae75f0be7cf57531e948ce6cf7d
dali <dali@itchy.cs.umu.se>
2015-04-29 14:57:17 +0200

    Added tests of genome release conversion, also restructured file structure for conversion data and results for these tests.

--- test/conversion/test/GenomeReleaseConverterTest.java
+++ test/conversion/test/GenomeReleaseConverterTest.java
@@ -2,20 +2,51 @@ package conversion.test;
 
 import org.junit.Ignore;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 import conversion.GenomeReleaseConverter;
 
-import java.io.IOException;
+import java.io.*;
+
 
 @Ignore
 public class GenomeReleaseConverterTest {
 
 
-	// TODO: This seems to always succeed no matter whether the conversion happened or not.
 	@Test
-	public void test() throws IOException, InterruptedException {
+	public void shouldRunLiftoverWithoutFailing()
+			throws IOException, InterruptedException {
 		GenomeReleaseConverter handler = new GenomeReleaseConverter();
-		handler.procedure("test.bed", "temp.bed", "dm2ToDm3.over.chain.gz", "unlifted.bed");
+		handler.procedure("genomeConversionTestData/test.bed",
+				"genomeConversionTestData/conversionResults/temp.bed",
+				"genomeConversionTestData/dm2ToDm3.over.chain.gz",
+				"genomeConversionTestData/conversionResults/unlifted.bed");
+	}
+
+	@Test
+	public void shouldCreateResultBedFile()
+			throws IOException, InterruptedException {
+		GenomeReleaseConverter handler = new GenomeReleaseConverter();
+		handler.procedure("genomeConversionTestData/test.bed",
+				"genomeConversionTestData/conversionResults/temp.bed",
+				"genomeConversionTestData/dm2ToDm3.over.chain.gz",
+				"genomeConversionTestData/conversionResults/unlifted.bed");
+		File resFile = new File("/resources/genomeConversionTestData/" +
+								"conversionResults/temp.bed");
+		assertTrue(resFile.exists());
+	}
+
+	@Test
+	public void shouldCreateUnliftedBedFile()
+		throws IOException, InterruptedException {
+		GenomeReleaseConverter handler = new GenomeReleaseConverter();
+		handler.procedure("genomeConversionTestData/test.bed",
+				"genomeConversionTestData/conversionResults/temp.bed",
+				"genomeConversionTestData/dm2ToDm3.over.chain.gz",
+				"genomeConversionTestData/conversionResults/unlifted.bed");
+		File resFile = new File("/resources/genomeConversionTestData/" +
+								"conversionResults/unlifted.bed");
+		assertTrue(resFile.exists());
 	}
 
 
