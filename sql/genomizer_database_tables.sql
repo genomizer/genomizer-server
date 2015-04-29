5b460bcce7162d8882204286cdb8f31687b8f0bc
Erik Samuelsson <dv13esn@cs.umu.se>
2015-04-29 10:18:25 +0200

    Added Relation: Parent. For file traceability.

--- sql/genomizer_database_tables.sql
+++ sql/genomizer_database_tables.sql
@@ -12,12 +12,21 @@ CREATE TABLE File
     IsPrivate       BOOLEAN         NOT NULL,
     ExpID           VARCHAR(256),
     GRVersion       VARCHAR(16),
-    Status          VARCHAR(16)   DEFAULT 'In Progress',
+    Status          VARCHAR(16)     DEFAULT 'In Progress',
     ProcessName     VARCHAR(32),
     ProcessVersion  VARCHAR(32),
     ProcessFlags    VARCHAR(32),
     CONSTRAINT pkey_file PRIMARY KEY(FileID)
 );
+
+CREATE TABLE Parent
+{
+    FileID,
+    ParentID,
+    CONSTRAINT pkey_parent PRIMARY KEY(FileID, ParentID),
+    CONSTRAINT fkey_fileid FOREIGN KEY (FileID) REFERENCES File(FileID) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fkey_parentid FOREIGN KEY (ParentID) REFERENCES File(FileID) ON UPDATE CASCADE ON DELETE CASCADE
+};
 
 CREATE TABLE Annotation
 (
