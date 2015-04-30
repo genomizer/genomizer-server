f69cc256e22bfd33e8abff6264e8a104a01a8fc9
ens11afk <ens11afk@cs.umu.se>
2015-04-30 14:07:26 +0200

    Corrected some test errors for Travis

--- test/response/test/GetGenomeReleaseResponseTest.java
+++ test/response/test/GetGenomeReleaseResponseTest.java
@@ -23,90 +23,91 @@ import response.Response;
 import response.StatusCode;
 import server.ServerSettings;
 
+@Ignore
 public class GetGenomeReleaseResponseTest {
-//
-//	@Before
-//	public void setup() {
-//		TestInitializer.setupServerSettings();
-//	}
-//
-//	@Test
-//	public void testIfResponseCodeIsCorrectForExisting() {
-//
-//
-//
-//		//ArrayList<Genome> genomeList;
-//		try {
-//
-//			new DatabaseAccessor(ServerSettings.databaseUsername, ServerSettings.databasePassword, ServerSettings.databaseHost, ServerSettings.databaseName);
-//			//genomeList = db.getAllGenomReleases();
-//			Command cmd=new GetGenomeReleaseSpeciesCommand("mouseTEST");
-//			Response rsp=cmd.execute();
-//			assertEquals(StatusCode.OK, rsp.getCode());
-//
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//
-//	}
-//
-//	// TODO: This returns 200 and an empty list instead of an error code. Not sure if correct or not.
-//	@Ignore
-//	@Test
-//	public void testIfResponseCodeIsCorrectNotExisting() {
-//
-//
-//
-//		//ArrayList<Genome> genomeList;
-//		try {
-//
-//			new DatabaseAccessor(ServerSettings.databaseUsername, ServerSettings.databasePassword, ServerSettings.databaseHost, ServerSettings.databaseName);
-//			//genomeList = db.getAllGenomReleases();
-//			Command cmd=new GetGenomeReleaseSpeciesCommand("hej");
-//			Response rsp=cmd.execute();
-//			assertEquals(StatusCode.BAD_REQUEST, rsp.getCode());
-//
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//
-//	}
-//
-//	@Test
-//	public void testPrintResponse() {
-//
-//
-//		ArrayList<database.containers.Genome> genomeList;
-//		try {
-//			DatabaseAccessor db=new DatabaseAccessor(ServerSettings.databaseUsername, ServerSettings.databasePassword, ServerSettings.databaseHost, ServerSettings.databaseName);
-//			genomeList = (ArrayList<database.containers.Genome>) db.getAllGenomReleases();
-//
-//		/*	for(int i=0; i<db.getAllAnnotationLabels().size();i++){
-//				System.out.println(db.getAllAnnotationLabels().get(i));
-//			}
-//			*/
-//			/*for(int i=0; i<db.getChoices("SpeciesTEST").size();i++){
-//				System.out.println(db.getChoices("SpeciesTEST"));
-//
-//			}*/
-//
-//			//genomeList =db.getAllGenomeReleasesForSpecies("Human");
-//			GetGenomeReleaseResponse gResp=new GetGenomeReleaseResponse(StatusCode.OK,genomeList);
-//			System.out.println(gResp.getBody());
-//
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//
-//	}
+
+	@Before
+	public void setup() {
+		TestInitializer.setupServerSettings();
+	}
+
+	@Test
+	public void testIfResponseCodeIsCorrectForExisting() {
+
+
+
+		//ArrayList<Genome> genomeList;
+		try {
+
+			new DatabaseAccessor(ServerSettings.databaseUsername, ServerSettings.databasePassword, ServerSettings.databaseHost, ServerSettings.databaseName);
+			//genomeList = db.getAllGenomReleases();
+			Command cmd=new GetGenomeReleaseSpeciesCommand();
+			Response rsp=cmd.execute();
+			assertEquals(StatusCode.OK, rsp.getCode());
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	// TODO: This returns 200 and an empty list instead of an error code. Not sure if correct or not.
+	@Ignore
+	@Test
+	public void testIfResponseCodeIsCorrectNotExisting() {
+
+
+
+		//ArrayList<Genome> genomeList;
+		try {
+
+			new DatabaseAccessor(ServerSettings.databaseUsername, ServerSettings.databasePassword, ServerSettings.databaseHost, ServerSettings.databaseName);
+			//genomeList = db.getAllGenomReleases();
+			Command cmd=new GetGenomeReleaseSpeciesCommand();
+			Response rsp=cmd.execute();
+			assertEquals(StatusCode.BAD_REQUEST, rsp.getCode());
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Test
+	public void testPrintResponse() {
+
+
+		ArrayList<database.containers.Genome> genomeList;
+		try {
+			DatabaseAccessor db=new DatabaseAccessor(ServerSettings.databaseUsername, ServerSettings.databasePassword, ServerSettings.databaseHost, ServerSettings.databaseName);
+			genomeList = (ArrayList<database.containers.Genome>) db.getAllGenomReleases();
+
+		/*	for(int i=0; i<db.getAllAnnotationLabels().size();i++){
+				System.out.println(db.getAllAnnotationLabels().get(i));
+			}
+			*/
+			/*for(int i=0; i<db.getChoices("SpeciesTEST").size();i++){
+				System.out.println(db.getChoices("SpeciesTEST"));
+
+			}*/
+
+			//genomeList =db.getAllGenomeReleasesForSpecies("Human");
+			GetGenomeReleaseResponse gResp=new GetGenomeReleaseResponse(StatusCode.OK,genomeList);
+			System.out.println(gResp.getBody());
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
