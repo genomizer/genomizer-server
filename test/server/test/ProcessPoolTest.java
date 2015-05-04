7950dbd279cbefd8f596eb1ae373b5e540702524
tastyburger <punisher66@live.com>
2015-05-04 11:42:30 +0200

    Minor change

---
+++ test/server/test/ProcessPoolTest.java
@@ -0,0 +1,18 @@
+package server.test;
+
+
+import org.junit.Before;
+import server.ProcessPool;
+
+
+public class ProcessPoolTest {
+
+    private ProcessPool processPool;
+
+    @Before
+    public void setUp() {
+        processPool = new ProcessPool();
+    }
+
+
+}
