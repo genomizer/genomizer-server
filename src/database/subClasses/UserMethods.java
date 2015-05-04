d122c439e9e8f4a19de8e8e36a742541bcf22c17
Erik Samuelsson <dv13esn@cs.umu.se>
2015-05-04 09:56:27 +0200

    Added method to update user info.

--- src/database/subClasses/UserMethods.java
+++ src/database/subClasses/UserMethods.java
@@ -12,295 +12,290 @@ import java.util.List;
 /**
  * Class that contains all the methods for adding,changing, getting and removing
  * Users in the database. This class is a subClass of databaseAcessor.java
- *
+ * <p/>
  * date: 2014-05-14 version: 1.0
  */
 public class UserMethods {
 
-	private Connection conn;
+    private Connection conn;
 
-	public UserMethods(Connection connection) {
+    public UserMethods(Connection connection) {
 
-		conn = connection;
-	}
+        conn = connection;
+    }
 
-	/**
-	 * Returns an ArrayList which contains the usernames of all the users in the
-	 * database in the form of strings.
-	 *
-	 * @return an ArrayList of usernames.
-	 * @throws SQLException
-	 *             if the query does not succeed
-	 */
-	public List<String> getUsers() throws SQLException {
+    /**
+     * Returns an ArrayList which contains the usernames of all the users in the
+     * database in the form of strings.
+     *
+     * @return an ArrayList of usernames.
+     * @throws SQLException if the query does not succeed
+     */
+    public List<String> getUsers() throws SQLException {
 
-		ArrayList<String> usersList = new ArrayList<String>();
-		String query = "SELECT Username FROM User_Info";
+        ArrayList<String> usersList = new ArrayList<String>();
+        String query = "SELECT Username FROM User_Info";
 
-		Statement stmt = conn.createStatement();
-		ResultSet rs = stmt.executeQuery(query);
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(query);
 
-		while (rs.next()) {
-			usersList.add(rs.getString("Username"));
-		}
+        while (rs.next()) {
+            usersList.add(rs.getString("Username"));
+        }
 
-		stmt.close();
+        stmt.close();
 
-		return usersList;
-	}
+        return usersList;
+    }
 
-	/**
-	 * Adds a new user to the database.
-	 *
-	 * @param username the username of the user
-	 * @param passwordHash the hash of the user's password and salt
-	 * @param passwordSalt the user's salt
-	 * @param role role given to the user ie. "Admin"
-	 * @param fullName the full name of the user
-	 * @param email email address of the new user.
-	 * @throws SQLException
-	 * @throws IOException if an argument string is empty
-	 */
-	public void addUser(String username, String passwordHash, String passwordSalt, String role,
-			String fullName, String email) throws SQLException, IOException {
+    /**
+     * Adds a new user to the database.
+     *
+     * @param username     the username of the user
+     * @param passwordHash the hash of the user's password and salt
+     * @param role         role given to the user ie. "Admin"
+     * @param fullName     the full name of the user
+     * @param email        email address of the new user.
+     * @throws SQLException
+     * @throws IOException  if an argument string is empty
+     */
+    public void addUser(String username, String passwordHash, String role,
+                        String fullName, String email) throws SQLException, IOException {
 
-		isValidArgument(username);
-		isValidArgument(passwordHash);
-		isValidArgument(passwordSalt);
-		isValidArgument(role);
-		isValidArgument(fullName);
-		isValidArgument(email);
+        isValidArgument(username);
+        isValidArgument(passwordHash);
+        isValidArgument(role);
+        isValidArgument(fullName);
+        isValidArgument(email);
 
-		String query = "INSERT INTO User_Info (Username, PasswordHash, PasswordSalt, Role, FullName, Email) "
-				+ "VALUES " + "(?, ?, ?, ?, ?, ?)";
+        String query = "INSERT INTO User_Info (Username, PasswordHash, Role, FullName, Email) "
+                + "VALUES " + "(?, ?, ?, ?, ?)";
 
-		PreparedStatement stmt = conn.prepareStatement(query);
-		stmt.setString(1, username);
-		stmt.setString(2, passwordHash);
-		stmt.setString(3, passwordSalt);
-		stmt.setString(4, role);
-		stmt.setString(5, fullName);
-		stmt.setString(6, email);
-		stmt.executeUpdate();
-		stmt.close();
-	}
+        PreparedStatement stmt = conn.prepareStatement(query);
+        stmt.setString(1, username);
+        stmt.setString(2, passwordHash);
+        stmt.setString(3, role);
+        stmt.setString(4, fullName);
+        stmt.setString(5, email);
+        stmt.executeUpdate();
+        stmt.close();
+    }
 
-	/**
-	 * Deletes a user from the database.
-	 *
-	 * @param username
-	 *            the username of the user to be deleted.
-	 * @throws SQLException
-	 *             if the query does not succeed
-	 */
-	public int deleteUser(String username) throws SQLException {
+    /**
+     * Deletes a user from the database.
+     *
+     * @param username the username of the user to be deleted.
+     * @throws SQLException if the query does not succeed
+     */
+    public int deleteUser(String username) throws SQLException {
 
-		String query = "DELETE FROM User_Info " + "WHERE (Username = ?)";
+        String query = "DELETE FROM User_Info " + "WHERE (Username = ?)";
 
-		PreparedStatement stmt = conn.prepareStatement(query);
-		stmt.setString(1, username);
-		int res = stmt.executeUpdate();
-		stmt.close();
-		return res;
-	}
+        PreparedStatement stmt = conn.prepareStatement(query);
+        stmt.setString(1, username);
+        int res = stmt.executeUpdate();
+        stmt.close();
+        return res;
+    }
 
-	/**
-	 * Returns the password hash for the given user. Used for login.
-	 *
-	 * @param user
-	 *            - the username as string
-	 * @return String - the password hash
-	 * @throws SQLException
-	 *             if the query does not succeed
-	 */
-	public String getPasswordHash(String user) throws SQLException {
+    /**
+     * Returns the password hash for the given user. Used for login.
+     *
+     * @param user - the username as string
+     * @return String - the password hash
+     * @throws SQLException if the query does not succeed
+     */
+    public String getPasswordHash(String user) throws SQLException {
 
-		String query = "SELECT PasswordHash FROM User_Info "
-				+ "WHERE (Username = ?)";
+        String query = "SELECT PasswordHash FROM User_Info "
+                + "WHERE (Username = ?)";
 
-		PreparedStatement stmt = conn.prepareStatement(query);
-		stmt.setString(1, user);
-		ResultSet rs = stmt.executeQuery();
-		String pass = null;
+        PreparedStatement stmt = conn.prepareStatement(query);
+        stmt.setString(1, user);
+        ResultSet rs = stmt.executeQuery();
+        String pass = null;
 
-		if (rs.next()) {
-			pass = rs.getString("passwordHash");
-		}
+        if (rs.next()) {
+            pass = rs.getString("passwordHash");
+        }
 
-		stmt.close();
+        stmt.close();
 
-		return pass;
-	}
+        return pass;
+    }
 
-	/**
-	 * Returns the password salt for the given user. Used for login.
-	 *
-	 * @param user
-	 *            - the username as string
-	 * @return String - the password salt
-	 * @throws SQLException
-	 *             if the query does not succeed
-	 */
-	public String getPasswordSalt(String user) throws SQLException {
+    /**
+     * Changes the password for a user.
+     *
+     * @param username      the user to change the password for
+     * @param newPasswdHash the new password/salt hash
+     * @param newSalt       the salt for the new password
+     * @return the number of tuples updated in the database
+     * @throws SQLException if the query does not succeed
+     * @throws IOException
+     */
+    public int resetPassword(String username, String newPasswdHash, String newSalt)
+            throws SQLException, IOException {
 
-		String query = "SELECT PasswordSalt FROM User_Info "
-				+ "WHERE (Username = ?)";
+        if (username == null || username.contentEquals("") ||
+                newPasswdHash == null || newPasswdHash.contentEquals("")) {
+            throw new IOException("Invalid arguments");
+        }
 
-		PreparedStatement stmt = conn.prepareStatement(query);
-		stmt.setString(1, user);
-		ResultSet rs = stmt.executeQuery();
-		String pass = null;
+        String query = "UPDATE User_Info SET PasswordHash = ?"
+                + "WHERE (Username = ?)";
 
-		if (rs.next()) {
-			pass = rs.getString("passwordSalt");
-		}
+        PreparedStatement stmt = conn.prepareStatement(query);
+        stmt.setString(1, newPasswdHash);
+        stmt.setString(2, username);
+        int resCount = stmt.executeUpdate();
+        stmt.close();
 
-		stmt.close();
+        return resCount;
+    }
 
-		return pass;
-	}
+    /**
+     * Gets the role (permissions) for a user.
+     *
+     * @param username the user to get the role for.
+     * @return the role as a string.
+     * @throws SQLException if the query does not succeed
+     */
+    public String getRole(String username) throws SQLException {
 
-	/**
-	 * Changes the password for a user.
-	 *
-	 * @param username the user to change the password for
-	 * @param newPasswdHash the new password/salt hash
-	 * @param newSalt the salt for the new password
-	 * @return the number of tuples updated in the database
-	 * @throws SQLException if the query does not succeed
-	 * @throws IOException
-	 */
-	public int resetPassword(String username, String newPasswdHash, String newSalt)
-			throws SQLException, IOException {
+        String query = "SELECT Role FROM User_Info " + "WHERE (Username = ?)";
 
-		if (username == null || username.contentEquals("") ||
-				newPasswdHash == null || newPasswdHash.contentEquals("")) {
-			throw new IOException("Invalid arguments");
-		}
+        PreparedStatement stmt = conn.prepareStatement(query);
+        stmt.setString(1, username);
+        ResultSet rs = stmt.executeQuery();
+        String role = null;
 
-		String query = "UPDATE User_Info SET PasswordHash = ?, PasswordSalt = ?"
-				+ "WHERE (Username = ?)";
+        if (rs.next()) {
+            role = rs.getString("Role");
+        }
 
-		PreparedStatement stmt = conn.prepareStatement(query);
-		stmt.setString(1, newPasswdHash);
-		stmt.setString(2, newSalt);
-		stmt.setString(3, username);
-		int resCount = stmt.executeUpdate();
-		stmt.close();
+        stmt.close();
 
-		return resCount;
-	}
+        return role;
+    }
 
-	/**
-	 * Gets the role (permissions) for a user.
-	 *
-	 * @param username
-	 *            the user to get the role for.
-	 * @return the role as a string.
-	 * @throws SQLException
-	 *             if the query does not succeed
-	 */
-	public String getRole(String username) throws SQLException {
+    /**
+     * Update a user's info. Changes a user's Role, Full Name
+     * and Email in the database.
+     *
+     * @param username - The user to update.
+     * @param role     - The user's new role.
+     * @param fullName - The user's full name.
+     * @param email    - The user's email.
+     * @return the number of tuples updated in the database.
+     */
+    public int updateUser(String username, String role, String fullName,
+                          String email) throws SQLException, IOException {
+        isValidArgument(username);
+        isValidArgument(role);
+        isValidArgument(fullName);
+        isValidArgument(email);
 
-		String query = "SELECT Role FROM User_Info " + "WHERE (Username = ?)";
+        String query = "UPDATE User_Info" +
+                "SET Role = ?, FullName = ?, Email = ?" +
+                "WHERE UserName = ?";
 
-		PreparedStatement stmt = conn.prepareStatement(query);
-		stmt.setString(1, username);
-		ResultSet rs = stmt.executeQuery();
-		String role = null;
+        PreparedStatement stmt = conn.prepareStatement(query);
+        stmt.setString(1, role);
+        stmt.setString(2, fullName);
+        stmt.setString(3, email);
+        stmt.setString(4, username);
+        int result = stmt.executeUpdate();
+        stmt.close();
+        return result;
+    }
 
-		if (rs.next()) {
-			role = rs.getString("Role");
-		}
+    /**
+     * Deprecated: Use updateUser(String username, String role, String fullName,
+     * String email) instead.
+     *
+     * Sets the role (permissions) for the user.
+     *
+     * @param username the user to set the role for.
+     * @param role     the role to set for the user.
+     * @return returns the number of tuples updated in the database.
+     * @throws SQLException if the query does not succeed
+     */
+    @Deprecated
+    public int setRole(String username, String role) throws SQLException {
 
-		stmt.close();
+        String query = "UPDATE User_Info SET Role = ? "
+                + "WHERE (Username = ?)";
 
-		return role;
-	}
+        PreparedStatement stmt = conn.prepareStatement(query);
+        stmt.setString(1, role);
+        stmt.setString(2, username);
 
-	/**
-	 * Sets the role (permissions) for the user.
-	 *
-	 * @param username
-	 *            the user to set the role for.
-	 * @param role
-	 *            the role to set for the user.
-	 * @return returns the number of tuples updated in the database.
-	 * @throws SQLException
-	 *             if the query does not succeed
-	 */
-	public int setRole(String username, String role) throws SQLException {
+        int resCount = stmt.executeUpdate();
+        stmt.close();
 
-		String query = "UPDATE User_Info SET Role = ? "
-				+ "WHERE (Username = ?)";
+        return resCount;
+    }
 
-		PreparedStatement stmt = conn.prepareStatement(query);
-		stmt.setString(1, role);
-		stmt.setString(2, username);
+    /**
+     * Gets the full name of a user.
+     *
+     * @param username the user to lookup
+     * @return a string containing the full name or null
+     * @throws SQLException
+     */
+    public String getUserFullName(String username) throws SQLException {
 
-		int resCount = stmt.executeUpdate();
-		stmt.close();
+        String query = "SELECT FullName FROM User_Info " +
+                "WHERE (Username = ?)";
 
-		return resCount;
-	}
+        PreparedStatement stmt = conn.prepareStatement(query);
+        stmt.setString(1, username);
 
-	/**
-	 * Gets the full name of a user.
-	 * @param username the user to lookup
-	 * @return a string containing the full name or null
-	 * @throws SQLException
-	 */
-	public String getUserFullName(String username) throws SQLException {
+        String name = null;
+        ResultSet rs = stmt.executeQuery();
 
-		String query = "SELECT FullName FROM User_Info " +
-				"WHERE (Username = ?)";
+        if (rs.next()) {
+            name = rs.getString("FullName");
+        }
 
-		PreparedStatement stmt = conn.prepareStatement(query);
-		stmt.setString(1,username);
+        stmt.close();
 
-		String name = null;
-		ResultSet rs = stmt.executeQuery();
+        return name;
+    }
 
-		if(rs.next()){
-			name = rs.getString("FullName");
-		}
+    /**
+     * Gets a user's email.
+     *
+     * @param username the user to lookup
+     * @return a string containing the user's email or null
+     * @throws SQLException
+     */
+    public String getUserEmail(String username) throws SQLException {
 
-		stmt.close();
+        String query = "SELECT Email FROM User_Info " +
+                "WHERE (Username = ?)";
 
-		return name;
-	}
+        PreparedStatement stmt = conn.prepareStatement(query);
+        stmt.setString(1, username);
 
-	/**
-	 * Gets a user's email.
-	 * @param username the user to lookup
-	 * @return a string containing the user's email or null
-	 * @throws SQLException
-	 */
-	public String getUserEmail(String username) throws SQLException {
+        String email = null;
+        ResultSet rs = stmt.executeQuery();
 
-		String query = "SELECT Email FROM User_Info " +
-				"WHERE (Username = ?)";
+        if (rs.next()) {
+            email = rs.getString("Email");
+        }
 
-		PreparedStatement stmt = conn.prepareStatement(query);
-		stmt.setString(1,username);
+        stmt.close();
 
-		String email = null;
-		ResultSet rs = stmt.executeQuery();
-
-		if(rs.next()){
-			email = rs.getString("Email");
-		}
-
-		stmt.close();
-
-		return email;
-	}
+        return email;
+    }
 
 
-	private void isValidArgument(String arg) throws IOException {
+    private void isValidArgument(String arg) throws IOException {
 
-    	if (arg == null || arg.contentEquals("")) {
-    		throw new IOException("Invalid argument(s)");
-    	}
+        if (arg == null || arg.contentEquals("")) {
+            throw new IOException("Invalid argument(s)");
+        }
     }
 }
