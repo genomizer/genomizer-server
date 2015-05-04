dbe3e1dff8712025ec942a306cb9fdb52f35f337
Erik Samuelsson <dv13esn@cs.umu.se>
2015-05-04 09:56:59 +0200

    Removed field for password salt since it is not used anymore.

--- sql/genomizer_database_tables.sql
+++ sql/genomizer_database_tables.sql
@@ -56,7 +56,6 @@ CREATE TABLE User_Info
 (
     Username VARCHAR(32) NOT NULL,
     PasswordHash VARCHAR(64) NOT NULL,
-    PasswordSalt VARCHAR(64) NOT NULL,
     Role VARCHAR(32) NOT NULL,
     FullName VARCHAR(62),
     Email VARCHAR(62),
