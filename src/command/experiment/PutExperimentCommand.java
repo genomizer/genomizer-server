7b0e5737744364a19e9b809a4365590befc6cbe4
ens11afk <ens11afk@cs.umu.se>
2015-05-15 13:46:43 +0200

    Continued with the implementation.

--- src/command/experiment/PutExperimentCommand.java
+++ src/command/experiment/PutExperimentCommand.java
@@ -1,12 +1,21 @@
 package command.experiment;
 
+import com.google.gson.annotations.Expose;
+import command.Annotation;
 import command.Command;
 import command.UserRights;
 import command.ValidateException;
+import database.DatabaseAccessor;
+import database.constants.MaxLength;
 import database.subClasses.UserMethods.UserType;
+import response.ErrorResponse;
 import response.HttpStatusCode;
 import response.MinimalResponse;
 import response.Response;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  * Class used to handle the updating of the experiments.
@@ -17,17 +26,23 @@ import response.Response;
 public class PutExperimentCommand extends Command {
 	//TODO Implement this class
 
+	@Expose
+	private String name = null;
+
+	@Expose
+	private ArrayList<Annotation> annotations = new ArrayList<>();
+
 	private String expID;
+
+	@Override
+	public int getExpectedNumberOfURIFields() {
+		return 2;
+	}
 
 	@Override
 	public void setFields(String uri, String uuid, UserType userType) {
 		super.setFields(uri,uuid,userType);
 		expID = uri.split("/")[2];
-	}
-
-	@Override
-	public int getExpectedNumberOfURIFields() {
-		return 2;
 	}
 
 	/**
@@ -37,11 +52,47 @@ public class PutExperimentCommand extends Command {
 	@Override
 	public void validate() throws ValidateException {
 		hasRights(UserRights.getRights(this.getClass()));
+		validateName(name, MaxLength.EXPID, "Experiment name");
+
+		if(annotations == null || annotations.size() < 1) {
+			throw new ValidateException(HttpStatusCode.BAD_REQUEST, "Specify " +
+					"annotations for the experiment.");
+		}
+
+		for(int i =0;i<annotations.size();i++){
+			if(annotations.get(i) == null){
+				throw new ValidateException(HttpStatusCode.BAD_REQUEST, "Found " +
+						"an empty annotation or annotation value, please " +
+						"specify annotations.");
+			}
+			validateName(annotations.get(i).getName(),
+					MaxLength.ANNOTATION_LABEL, "Annotation label");
+			validateName(annotations.get(i).getValue(),
+					MaxLength.ANNOTATION_VALUE, "Annotation value");
+		}
 	}
 
 	@Override
 	public Response execute() {
-		return 	new MinimalResponse(HttpStatusCode.METHOD_NOT_YET_IMPLEMENTED);
+		DatabaseAccessor db = null;
+		try {
+			db = initDB();
+			if (!db.hasExperiment(expID)){
+				//Check things
+			}
+			for(Annotation annotation: annotations) {
+				db.updateExperiment(name, annotation.getName(),
+						annotation.getValue());
+			}
+			return new MinimalResponse(HttpStatusCode.OK);
+		} catch (IOException | SQLException e) {
+			e.printStackTrace();
+			return new ErrorResponse(HttpStatusCode.BAD_REQUEST, e.getMessage());
+		} finally {
+			if (db != null) {
+				db.close();
+			}
+		}
 	}
 
 }
