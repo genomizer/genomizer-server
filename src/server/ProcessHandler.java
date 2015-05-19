a5f04cb89d5b147fd0d05bc140e1615486c488c0
tastyburger <punisher66@live.com>
2015-05-19 12:03:05 +0200

    Fix output

--- src/server/ProcessHandler.java
+++ src/server/ProcessHandler.java
@@ -78,12 +78,14 @@ public class ProcessHandler implements Callable<Response> {
 			// TODO: for simulation only, should be removed for production
 			/* Long time process execution simulation */
 			ErrorLogger.log("PROCESS", "Process is sleeping for 30 seconds.");
-			Debug.log("Process is sleeping for 30 seconds.");
+			Debug.log("Process is sleeping for 30 seconds. PID " +
+					processCommand.getPID());
 			try {
 				Thread.sleep(30000);
 			} catch (InterruptedException ex) {
 				Debug.log("Sleep interrupted");
 			}
+			Debug.log("End of sleep. PID " + processCommand.getPID());
 
 
 			process.timeFinished = System.currentTimeMillis();
