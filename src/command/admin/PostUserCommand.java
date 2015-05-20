37a1e36063cc2f33587c93aa685b808b375b02d1
Mikael Karlsson <mikaelhakarlsson@gmail.com>
2015-05-20 10:10:26 +0200

    Minor refactoring.

--- src/command/admin/PostUserCommand.java
+++ src/command/admin/PostUserCommand.java
@@ -19,7 +19,7 @@ import response.Response;
 import server.Debug;
 
 /**
- * command used to create a user.
+ * Command used to create a user.
  *
  * @author Business Logic 2015.
  * @version 1.1
@@ -45,15 +45,10 @@ public class PostUserCommand extends Command {
 		return 2;
 
 	}
-	/**
-	 * Used to make sure the strings of the command are correct
-	 * @throws command.ValidateException
-	 */
+
 	@Override
 	public void validate() throws ValidateException {
-
 		hasRights(UserRights.getRights(this.getClass()));
-
 		validateName(username, MaxLength.USERNAME, "User");
 		validateName(password, MaxLength.PASSWORD, "Password");
 		validateName(privileges, MaxLength.ROLE, "Privileges");
@@ -61,35 +56,33 @@ public class PostUserCommand extends Command {
 		validateExists(email, MaxLength.EMAIL, "Email");
 	}
 
-	/**
-	 * Runs the command. The user gets added to the database.
-	 * @return a MinimalResponse or ErrorResponse
-	 */
 	@Override
 	public Response execute() {
 		DatabaseAccessor db;
 		try {
 			db = initDB();
 		} catch (SQLException | IOException e) {
-			Debug.log("Creation of user: " + username + " didn't work, reason: " +
-					e.getMessage());
-			return new ErrorResponse(HttpStatusCode.INTERNAL_SERVER_ERROR, "Creation of user: " + username +
-					" didn't work because of temporary problems with database.");
+			Debug.log("Creation of user: " + username + " was unsuccessful, " +
+					"reason: " + e.getMessage());
+			return new ErrorResponse(HttpStatusCode.INTERNAL_SERVER_ERROR,
+					"Creation of user: " + username + " was unsuccessful due " +
+							"to temporary problems with the database.");
 		}
+
 		try {
 			String hash = BCrypt.hashpw(password,BCrypt.gensalt());
-			db.addUser(username, hash, "SALT",privileges, name, email);
-
+			db.addUser(username, hash, "SALT", privileges, name, email);
 		} catch (SQLException | IOException e) {
-			Debug.log("Creation of user: " + username + " didn't work, reason: " +
-					e.getMessage());
-			return new ErrorResponse(HttpStatusCode.INTERNAL_SERVER_ERROR, "Creation of user: " + username +
-					" didn't work because of temporary problems with database.");
-		}finally {
-			db.close();
+			Debug.log("Creation of user: " + username + " was unsuccessful, " +
+					"reason: " +  e.getMessage());
+			return new ErrorResponse(HttpStatusCode.INTERNAL_SERVER_ERROR,
+					"Creation of user: " + username + "was unsuccessful due " +
+							"to temporary problems with the database.");
+		} finally {
+			if (db != null)
+				db.close();
 		}
+
 		return new MinimalResponse(HttpStatusCode.OK);
-
 	}
-
 }
