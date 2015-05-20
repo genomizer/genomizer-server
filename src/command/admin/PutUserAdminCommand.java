b97fb07c4b7d69fa18de89b16f17fd4252bda4d5
Mikael Karlsson <mikaelhakarlsson@gmail.com>
2015-05-20 10:20:20 +0200

    Minor refactoring.

--- src/command/admin/PutUserAdminCommand.java
+++ src/command/admin/PutUserAdminCommand.java
@@ -13,28 +13,24 @@ import response.MinimalResponse;
 import response.Response;
 import response.HttpStatusCode;
 import server.Debug;
-import database.subClasses.UserMethods;
 import java.io.IOException;
 import java.sql.SQLException;
 
 /**
- * Admin command to change user information
- * Created by dv13jen on 2015-05-04.
+ * Command used to alter user information.
+ *
+ * @author Business Logic 2015
+ * @version 1.1
  */
 public class PutUserAdminCommand extends Command {
-
     @Expose
     private String username = null;
-
     @Expose
     private String password = null;
-
     @Expose
     private String privileges = null;
-
     @Expose
     private String name = null;
-
     @Expose
     private String email = null;
 
@@ -43,29 +39,15 @@ public class PutUserAdminCommand extends Command {
         return 2;
     }
 
-    /**
-     * Set the UserType. Uri and Uuid not used in this command.
-     * @param uri the URI from the http request.
-     * @param query
-     * @param uuid
-     * @param userType
-     */
     @Override
-    public void setFields(String uri, String query, String uuid, UserMethods.UserType userType) {
+    public void setFields(String uri, String query, String uuid, UserType userType) {
+        super.setFields(uri, query, uuid, userType);
         this.userType = userType;
-		/*No fields from the URI is needed, neither is the UUID. Dummy
-		implementation*/
     }
 
-    /**
-     * Used to make sure the strings of the command are correct
-     * @throws command.ValidateException
-     */
     @Override
     public void validate() throws ValidateException {
-
         hasRights(UserRights.getRights(this.getClass()));
-
         validateName(username, MaxLength.USERNAME, "User");
         validateName(password, MaxLength.PASSWORD, "Password");
         validateName(privileges, MaxLength.ROLE, "Privileges");
@@ -74,38 +56,35 @@ public class PutUserAdminCommand extends Command {
 
     }
 
-    /**
-     * Perform the action of the command and return a response.
-     * A connection to the database is established and the user
-     * is updated with the information given.
-     * @return a response of the result of the command.
-     */
     @Override
     public Response execute() {
-
         DatabaseAccessor db;
         try {
             db = initDB();
         } catch (SQLException | IOException e) {
-            Debug.log("Editing of user: " + username + " didn't work, reason: " +
-                    e.getMessage());
-            return new ErrorResponse(HttpStatusCode.INTERNAL_SERVER_ERROR, "Editing of user: " + username +
-                    " didn't work because of temporary problems with database.");
+            Debug.log("Editing of user: " + username + " was unsuccessful, " +
+                    "reason: " + e.getMessage());
+            return new ErrorResponse(HttpStatusCode.INTERNAL_SERVER_ERROR,
+                    "Editing of user: " + username + " was unsuccessful due " +
+                            "to temporary problems with database.");
         }
+
         try {
             String hash = BCrypt.hashpw(password, BCrypt.gensalt());
-            db.updateUser(username, hash, UserType.valueOf(privileges), name, email);
+            db.updateUser(username, hash, UserType.valueOf(privileges), name,
+                    email);
         } catch (SQLException | IOException e) {
-            Debug.log("Editing of user: " + username + " didn't work, reason: " +
-                    e.getMessage());
-            return new ErrorResponse(HttpStatusCode.INTERNAL_SERVER_ERROR, "Error when " +
-                    "editing user "+username+" in database, "+"" +
-                    "user probably don't exist. ");
-        }finally {
-            db.close();
-
+            Debug.log("Editing of user: " + username + " was unsuccessful, " +
+                    "reason: " + e.getMessage());
+            return new ErrorResponse(HttpStatusCode.INTERNAL_SERVER_ERROR,
+                    "Error when editing user " + username + " in database, " +
+                    "user may not exist. ");
+        } finally {
+            if (db != null) {
+                db.close();
+            }
         }
-        return new MinimalResponse(HttpStatusCode.OK);
 
+        return new MinimalResponse(HttpStatusCode.OK);
     }
 }
