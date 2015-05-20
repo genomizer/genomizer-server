4f6d706866435a3843f744592331914f65886ad1
Mikael Karlsson <mikaelhakarlsson@gmail.com>
2015-05-20 10:31:54 +0200

    Minor refactoring

--- src/command/admin/DeleteAdminUserCommand.java
+++ src/command/admin/DeleteAdminUserCommand.java
@@ -30,7 +30,8 @@ public class DeleteAdminUserCommand extends Command {
 	}
 
 	@Override
-	public void setFields(String uri, String query, String uuid, UserType userType) {
+	public void setFields(String uri, String query, String uuid,
+						  UserType userType) {
 		super.setFields(uri, query, uuid, userType);
 		username = uri.split("/")[2];
 	}
