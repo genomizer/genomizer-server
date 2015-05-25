3d5e6f51772d9a6accb1810a59c5adb1c5999eae
niklasf <niklasf@cs.umu.se>
2015-05-25 15:06:32 +0200

    process cmd adapter stuff for johannes eyes only

---
+++ test/command/test/processCommands/ProcessCommandsTest.java
@@ -0,0 +1,58 @@
+package command.test.processCommands;
+
+import com.google.gson.Gson;
+import command.process.*;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * File:        ProcessCommandsTest.java
+ * Author:      Niklas Fries
+ * Contact:     niklasf@cs.umu.se
+ * Date:        2015-05-22
+ */
+
+public class ProcessCommandsTest {
+
+    private final Gson gson = ProcessCommandAdapter.getProcessCommandGson();
+
+    @Test
+    public void shouldReturnListWithOneCommand() throws Exception {
+
+        String json =
+                "{\"expId\":\"not_an_expid\"," +
+                "\"processCommands\":[{\"type\":\"bowtie\"," +
+                "\"files\":[{\"infile\":" +
+                "\"bigtest1.fastq\",\"outfile\":\"awsd\"," +
+                "\"genomeVersion\":\"theGR\",\"params\":\"" +
+                "-a -m 1 --best -p 10 -v 2 -q -S\",\"keepSam\":\"on\"}]}]}";
+
+        ProcessCommands_new processCommands =
+                gson.fromJson(json, ProcessCommands_new.class);
+
+        assertEquals(1, processCommands.getProcessCommands().size());
+        System.out.println("processCommands = " + processCommands);
+    }
+
+    @Test
+    public void shouldContailOneBowtieAndOneRatio() throws Exception {
+        String json =
+                "{\"expId\":\"not_an_expid\"," +
+                "\"processCommands\":[{\"type\":\"bowtie\"," +
+                "\"files\":[{\"infile\":\"bigtest1.fastq\"," +
+                "\"outfile\":\"awsd\",\"genomeVersion\":\"theGR\"," +
+                "\"params\":\"-a -m 1 --best -p 10 -v 2 -q -S\"," +
+                "\"keepSam\":\"on\"}]}, {\"type\":\"ratio\", \"infile1\": " +
+                "\"infile1Name\", \"infile2\": \"infile2Name\"}]}";
+
+        ProcessCommands_new processCommands =
+                gson.fromJson(json, ProcessCommands_new.class);
+        assertEquals(
+                BowtieProcessCommand.class,
+                processCommands.getProcessCommands().get(0).getClass());
+        assertEquals(
+                RatioProcessCommand.class,
+                processCommands.getProcessCommands().get(1).getClass());
+    }
+}
