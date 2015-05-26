b8b23b3442bd155f335199b45ad4b69d0f3de079
Martin Larsson <dv13mln@cs.umu.se>
2015-05-26 15:32:48 +0200

    Removed try-block in addExperiment()

--- src/database/subClasses/ExperimentMethods.java
+++ src/database/subClasses/ExperimentMethods.java
@@ -91,15 +91,11 @@ public class ExperimentMethods {
         fpg.generateExperimentFolders(expID);
         String query = "INSERT INTO Experiment "
                 + "(ExpID) VALUES (?)";
-        int rs = 0;
-        try (PreparedStatement stmt= conn.prepareStatement(query);) {
-            stmt.setString(1, expID);
-            rs = stmt.executeUpdate();
-            stmt.close();
-        } catch (SQLException ex) {
-            conn.rollback();
-            throw ex;
-        }
+        PreparedStatement stmt= conn.prepareStatement(query);
+        stmt.setString(1, expID);
+        int rs = stmt.executeUpdate();
+        stmt.close();
+
         return rs;
     }
 
