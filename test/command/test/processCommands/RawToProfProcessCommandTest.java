45ee284d7f9f0d62bba8322386bbfa49c60c34a9
Johannes Ekman <ekman.johannes@gmail.com>
2015-05-27 14:33:40 +0200

    Created new test class

---
+++ test/command/test/processCommands/RawToProfProcessCommandTest.java
@@ -0,0 +1,124 @@
+package command.test.processCommands;
+
+import com.google.gson.Gson;
+import command.ValidateException;
+import command.process.ProcessCommands;
+import database.constants.MaxLength;
+import database.subClasses.UserMethods;
+import org.junit.Test;
+import server.RequestHandler;
+
+import static org.junit.Assert.*;
+
+public class RawToProfProcessCommandTest {
+    private final Gson gson = new RequestHandler().getGson();
+
+    @Test(expected = ValidateException.class)
+    public void canGiveValidateExceptionOnIncorrectInfileName() throws ValidateException {
+        String json =
+                "{\"expId\":\"asd\"," +
+                        "\"processCommands\":[{\"type\":\"rawToProfile\"," +
+                        "\"files\":[{\"infile\":\"bigtes&t1.fastq\"," +
+                        "\"outfile\":\"awsd\",\"genomeVersion\":\"theGR\"," +
+                        "\"params\":\"-a -m 1 --best -p 10 -v 2 -q -S\"," +
+                        "\"keepSam\":\"on\"}]}, {\"type\":\"ratio\", \"infile1\": " +
+                        "\"infile1Name\", \"infile2\": \"infile2Name\"}]}";
+        ProcessCommands processCommands =
+                gson.fromJson(json, ProcessCommands.class);
+        processCommands.setFields(null, null, null, UserMethods.UserType.USER);
+        processCommands.validate();
+    }
+
+    @Test (expected = ValidateException.class)
+    public void shouldGiveValidateExceptionOnIncorrectInfileLength() throws ValidateException {
+
+        String s = "";
+        for(int i = 0; i < MaxLength.FILE_FILENAME + 1; i++) {
+            s += "a";
+        }
+        String json =
+                "{\"expId\":\"asdws\"," +
+                        "\"processCommands\":[{\"type\":\"rawToProfile\"," +
+                        "\"files\":[{\"infile\":\""+s+"\"," +
+                        "\"outfile\":\"awsd\",\"genomeVersion\":\"theGR\"," +
+                        "\"params\":\"-a -m 1 --best -p 10 -v 2 -q -S\"," +
+                        "\"keepSam\":\"on\"}]}, {\"type\":\"ratio\", \"infile1\": " +
+                        "\"infile1Name\", \"infile2\": \"infile2Name\"}]}";
+        ProcessCommands processCommands =
+                gson.fromJson(json, ProcessCommands.class);
+        processCommands.setFields(null, null, null, UserMethods.UserType.USER);
+        processCommands.validate();
+    }
+
+    @Test (expected = ValidateException.class)
+    public void canGiveValidateExceptionOnIncorrectOutfileName() throws ValidateException {
+        String json =
+                "{\"expId\":\"asd\"," +
+                        "\"processCommands\":[{\"type\":\"rawToProfile\"," +
+                        "\"files\":[{\"infile\":\"bigtest1.fastq\"," +
+                        "\"outfile\":\"a%wsd\",\"genomeVersion\":\"theGR\"," +
+                        "\"params\":\"-a -m 1 --best -p 10 -v 2 -q -S\"," +
+                        "\"keepSam\":\"on\"}]}, {\"type\":\"ratio\", \"infile1\": " +
+                        "\"infile1Name\", \"infile2\": \"infile2Name\"}]}";
+        ProcessCommands processCommands =
+                gson.fromJson(json, ProcessCommands.class);
+        processCommands.setFields(null, null, null, UserMethods.UserType.USER);
+        processCommands.validate();
+    }
+    @Test (expected = ValidateException.class)
+    public void shouldGiveValidateExceptionOnIncorrectOutfileLength() throws ValidateException {
+
+        String s = "";
+        for(int i = 0; i < MaxLength.FILE_FILENAME + 1; i++) {
+            s += "a";
+        }
+        String json =
+                "{\"expId\":\"asdws\"," +
+                        "\"processCommands\":[{\"type\":\"rawToProfile\"," +
+                        "\"files\":[{\"infile\":\"2asd2\"," +
+                        "\"outfile\":\""+s+"\",\"genomeVersion\":\"theGR\"," +
+                        "\"params\":\"-a -m 1 --best -p 10 -v 2 -q -S\"," +
+                        "\"keepSam\":\"on\"}]}, {\"type\":\"ratio\", \"infile1\": " +
+                        "\"infile1Name\", \"infile2\": \"infile2Name\"}]}";
+        ProcessCommands processCommands =
+                gson.fromJson(json, ProcessCommands.class);
+        processCommands.setFields(null, null, null, UserMethods.UserType.USER);
+        processCommands.validate();
+    }
+
+    @Test (expected = ValidateException.class)
+    public void canGiveValidateExceptionOnIncorrectGenomeVersionName() throws ValidateException {
+        String json =
+                "{\"expId\":\"asd\"," +
+                        "\"processCommands\":[{\"type\":\"rawToProfile\"," +
+                        "\"files\":[{\"infile\":\"bigtest1.fastq\"," +
+                        "\"outfile\":\"awsd\",\"genomeVersion\":\"the+R\"," +
+                        "\"params\":\"-a -m 1 --best -p 10 -v 2 -q -S\"," +
+                        "\"keepSam\":\"on\"}]}, {\"type\":\"ratio\", \"infile1\": " +
+                        "\"infile1Name\", \"infile2\": \"infile2Name\"}]}";
+        ProcessCommands processCommands =
+                gson.fromJson(json, ProcessCommands.class);
+        processCommands.setFields(null, null, null, UserMethods.UserType.USER);
+        processCommands.validate();
+    }
+    @Test (expected = ValidateException.class)
+    public void shouldGiveValidateExceptionOnIncorrectGenomeVersionLength() throws ValidateException {
+
+        String s = "";
+        for(int i = 0; i < MaxLength.FILE_FILENAME + 1; i++) {
+            s += "a";
+        }
+        String json =
+                "{\"expId\":\"asdws\"," +
+                        "\"processCommands\":[{\"type\":\"rawToProfile\"," +
+                        "\"files\":[{\"infile\":\"2asd2\"," +
+                        "\"outfile\":\"asdwww\",\"genomeVersion\":\""+s+"\"," +
+                        "\"params\":\"-a -m 1 --best -p 10 -v 2 -q -S\"," +
+                        "\"keepSam\":\"on\"}]}, {\"type\":\"ratio\", \"infile1\": " +
+                        "\"infile1Name\", \"infile2\": \"infile2Name\"}]}";
+        ProcessCommands processCommands =
+                gson.fromJson(json, ProcessCommands.class);
+        processCommands.setFields(null, null, null, UserMethods.UserType.USER);
+        processCommands.validate();
+    }
+}
