07c4effedb6f680f348b81fa24a6a243463ffc03
Tim Hedberg <dv13thg@cs.umu.se>
2015-05-27 13:39:00 +0200

    added exposed strings and validation to putfileCommand

--- src/command/file/PutFileCommand.java
+++ src/command/file/PutFileCommand.java
@@ -1,11 +1,15 @@
 package command.file;
 
+import com.google.gson.annotations.Expose;
 import command.Command;
 import command.UserRights;
 import command.ValidateException;
-import response.HttpStatusCode;
-import response.MinimalResponse;
-import response.Response;
+import database.DatabaseAccessor;
+import database.constants.MaxLength;
+import response.*;
+
+import java.io.IOException;
+import java.sql.SQLException;
 
 /**
  * Class used to handle updating files in experiments.
@@ -14,7 +18,31 @@ import response.Response;
  * @version 1.1
  */
 public class PutFileCommand extends Command {
-	//TODO Implement this class
+	@Expose
+	private String fileName = null;
+
+	@Expose
+	private String type = null;
+
+	@Expose
+	private String metaData = null;
+
+	@Expose
+	private String author = null;
+
+	@Expose
+	private String grVersion = null;
+
+
+	@Override
+	public void validate() throws ValidateException {
+		hasRights(UserRights.getRights(this.getClass()));
+		validateName(type, MaxLength.FILE_FILETYPE, "File type");
+		validateName(author, MaxLength.FILE_AUTHOR, "Author");
+		validateName(grVersion, MaxLength.FILE_GRVERSION, "Genome release");
+		validateName(fileName, MaxLength.FILE_FILENAME, "Filename");
+		validateExists(metaData, MaxLength.FILE_METADATA, "Metadata");
+	}
 
 	@Override
 	public int getExpectedNumberOfURIFields() {
@@ -22,12 +50,14 @@ public class PutFileCommand extends Command {
 	}
 
 	@Override
-	public void validate() throws ValidateException {
-		hasRights(UserRights.getRights(this.getClass()));
-	}
-
-	@Override
 	public Response execute() {
+		try (DatabaseAccessor db = initDB()){
+			//TODO ADD db function here...
+
+		} catch (SQLException | IOException e) {
+			return new ErrorResponse(HttpStatusCode.INTERNAL_SERVER_ERROR, e.getMessage());
+		}
+
 		return 	new MinimalResponse(HttpStatusCode.NOT_IMPLEMENTED);
 	}
 }
