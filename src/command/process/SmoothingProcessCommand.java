3a09109be6c45b86070123bc96abf638b864732a
Johannes Ekman <ekman.johannes@gmail.com>
2015-06-01 09:35:22 +0200

    updated feedback information for different errors during processing

--- src/command/process/SmoothingProcessCommand.java
+++ src/command/process/SmoothingProcessCommand.java
@@ -140,22 +140,25 @@ public class SmoothingProcessCommand extends ProcessCommand {
             }else if(getMeanOrMedian().equals("median")){
                 meanOrMedian = 1;
             }else{
-                throw new UnsupportedOperationException("Error during smoothing processing. Incorrect mean/median. " +
-                        "Should be either 'mean' or 'median'.");
+                throw new UnsupportedOperationException("Error during smoothing processing on infile: "+infile+
+                        ". Incorrect mean/median. Should be either 'mean' or 'median'.");
             }
 
             try {
                 Smooth.runSmoothing(infileWithPath, getWindowSize(), meanOrMedian,  getMinSmooth(), 0 ,0, outfileWithPath);
 
             } catch (ValidateException e) {
-                Debug.log("Error during smoothing processing: "+e.getMessage());
-                throw new UnsupportedOperationException("Error during smoothing processing: "+e.getMessage());
+                Debug.log("Error during smoothing processing on infile: "+infile+ ". "+e.getMessage());
+                throw new UnsupportedOperationException("Error during smoothing processing on infile: "+infile+
+                        ". "+e.getMessage());
             } catch (IOException e) {
-                Debug.log("Error during smoothing processing: " + e.getMessage());
-                throw new UnsupportedOperationException("Error during smoothing processing: "+e.getMessage());
+                Debug.log("Error during smoothing processing on infile: "+infile+ ". "+e.getMessage());
+                throw new UnsupportedOperationException("Error during smoothing processing on infile: "+infile+
+                        ". "+e.getMessage());
             } catch (InterruptedException e) {
-                Debug.log("Error during smoothing processing: " + e.getMessage());
-                throw new UnsupportedOperationException("Error during smoothing processing: "+e.getMessage());
+                Debug.log("Error during smoothing processing on infile: "+infile+ ". "+e.getMessage());
+                throw new UnsupportedOperationException("Error during smoothing processing on infile: "+infile+
+                        ". "+e.getMessage());
             }
         }
     }
