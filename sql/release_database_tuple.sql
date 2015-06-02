aa5337d0d5a053f02785e20ceca8ba383ad5e12e
c12arr <c12arr@cs.umu.se>
2015-06-02 15:40:10 +0200

    Empty database, only contains users

---
+++ sql/release_database_tuple.sql
@@ -0,0 +1,5 @@
+INSERT INTO User_Info VALUES('yuri', '$2a$10$i31YyxPjtntIVvslbM.esOKv4TaWzssP.MkHw5DpOCs3NhJp4goKq', '$2a$10$i31YyxPjtntIVvslbM.esOKv4TaWzssP.MkHw5DpOCs3NhJp4goKq', 'ADMIN','Yuri Schwartz','yuri.schwartz@molbiol.umu.se');
+
+INSERT INTO User_Info VALUES('per', '$2a$10$i31YyxPjtntIVvslbM.esOKv4TaWzssP.MkHw5DpOCs3NhJp4goKq', '$2a$10$i31YyxPjtntIVvslbM.esOKv4TaWzssP.MkHw5DpOCs3NhJp4goKq', 'ADMIN','Per Stenberg','per.stenberg@molbiol.umu.se');
+
+INSERT INTO User_Info VALUES('jan', '$2a$10$i31YyxPjtntIVvslbM.esOKv4TaWzssP.MkHw5DpOCs3NhJp4goKq', '$2a$10$i31YyxPjtntIVvslbM.esOKv4TaWzssP.MkHw5DpOCs3NhJp4goKq', 'ADMIN','Jan Larsson','jan.larsson@molbiol.umu.se');
