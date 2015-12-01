ae9cf8c39acc8faad65ce766cf00ce82d61ab2f9
Mikhail Glushenkov <mikhail.glushenkov@gmail.com>
2015-12-01 14:03:18 +0100

    Remove unused backup scripts.

--- resources/backup/local_file_backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Synchronizes the server and the backup. The backup will be an image
-# of the current state of the server.
-
-READPATH=
-SAVEPATH=
-rsync --delete --update -avz $READPATH $SAVEPATH
